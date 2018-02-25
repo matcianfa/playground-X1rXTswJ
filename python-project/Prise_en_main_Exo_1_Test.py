e7bd94256f2befcaad79ba1abac1883d5bfc93b1
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-25 17:45:10 +0100

    Update Prise_en_main_Exo_1_Test.py

--- python-project/Prise_en_main_Exo_1_Test.py
+++ python-project/Prise_en_main_Exo_1_Test.py
@@ -28,15 +28,15 @@ def fail():
 
 def test():
     try:
-		for inp,outp in input_output:
-			count1 = mon_programme(*inp)
-			assert count1 == outp, "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
-				success()
-	except AssertionError as e:
-		fail()
-		send_msg("Oops! ", e)
-        if help:
-        	send_msg("Aide 💡", help)
+      for inp,outp in input_output:
+        count1 = mon_programme(*inp)
+        assert count1 == outp, "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+          if help:
+            send_msg("Aide 💡", help)
 
 
 if __name__ == "__main__":
