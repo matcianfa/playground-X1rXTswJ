92968f6ff7a752ffb2b36821555edffa7b0fa349
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-25 23:33:30 +0100

    Create Calcul_du_produit_scalaire_Test.py

---
+++ python-project/Calcul_du_produit_scalaire_Test.py
@@ -0,0 +1,52 @@
+from Calcul_du_produit_scalaire import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+((1,2,3,4),11),\
+((-1,0,0,2),0),\
+((-1,4,1,-2),-9),\
+((-2,-3,3,-2),-0)\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__":
+  test()
