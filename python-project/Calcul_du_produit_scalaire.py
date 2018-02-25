7d00c22d2cb14720779bf1cd8bffd3fc763d2568
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-25 23:42:09 +0100

    Update Calcul_du_produit_scalaire.py

--- python-project/Calcul_du_produit_scalaire.py
+++ python-project/Calcul_du_produit_scalaire.py
@@ -1,5 +1,5 @@
 def mon_programme(x1,y1,x2,y2):
   #Ne pas toucher ce qui précède
-  #Les valeurs pour les variables seront automatiquement données
+  #Les valeurs pour les variables en entrée seront automatiquement données
   #Ecrire ci-dessous en n'oubliant pas d'indenter
   
