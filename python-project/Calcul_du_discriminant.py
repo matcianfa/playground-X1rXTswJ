066ee76239ff32d2b6f3c9a5648732695eb02d91
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-25 23:42:21 +0100

    Update Calcul_du_discriminant.py

--- python-project/Calcul_du_discriminant.py
+++ python-project/Calcul_du_discriminant.py
@@ -1,5 +1,5 @@
 def mon_programme(a,b,c):
     #Ne pas toucher ce qui précède
-    #Les valeurs pour les variables seront automatiquement données
+    #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
     
