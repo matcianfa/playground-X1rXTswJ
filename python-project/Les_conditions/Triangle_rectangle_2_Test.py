30ccb88d61074c1d7839488348210d8df27523ab
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-26 18:11:49 +0100

    Update Triangle_rectangle_2_Test.py

--- python-project/Les_conditions/Triangle_rectangle_2_Test.py
+++ python-project/Les_conditions/Triangle_rectangle_2_Test.py
@@ -8,7 +8,7 @@ import io
 input_output=[\
 ((0,0,5,0,0,6),"RECTANGLE"),\
 ((1,1,0.5,0.5,1,0),"RECTANGLE"),\
-((1,1,0,0,0,1),"PAS RECTANGLE"),\
+((1,1,0,0,2,1),"PAS RECTANGLE"),\
 ((1,1,0,1,0.5,0.5),"RECTANGLE"),\
 ((1,2,3,4,5,6),"PAS RECTANGLE")\
 ]
