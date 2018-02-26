ecb880135b15dd37b20c2bc40520b5d5647eb5ad
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-26 17:59:36 +0100

    Create Triangle_rectangle_2.py

---
+++ python-project/Les_conditions/Triangle_rectangle_2.py
@@ -0,0 +1,4 @@
+def mon_programme(x_a,y_a,x_b,y_b,x_c,y_c):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
