97bf57b69e61efed88ce66f6881ad2b99953dd94
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-26 18:26:36 +0100

    Create Triangle_rectangle_0_Test.py

---
+++ python-project/Les_conditions/Triangle_rectangle_0_Test.py
@@ -0,0 +1,53 @@
+#Ne pas oublier de changer le module à importer
+from Triangle_rectangle_0 import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+((90,80,10),"RECTANGLE"),\
+((120,30,30),"PAS RECTANGLE"),\
+((45,90,45),"RECTANGLE"),\
+((60,60,60),"PAS RECTANGLE"),\
+((30,60,90),"RECTANGLE")\
+]
+
+
+#message d'aide si besoin
+help="L'angle qui est droit n'est pas forcément le premier ou le dernier..."
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
