2a5b7d00cef1174a107f04489683bbc74e35fcf8
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-26 10:30:38 +0100

    Create Calcul_distance_vol_oiseau.py

---
+++ python-project/Variables et fonctions/Calcul_distance_vol_oiseau.py
@@ -0,0 +1,6 @@
+from math import *
+
+def mon_programme(lat_a,long_a,lat_b,long_b):
+    #Ne pas toucher ce qui précède.
+    #Les valeurs pour les variables en entrée seront automatiquement données.
+    #Ecrire ci-dessous en n'oubliant pas d'indenter.
