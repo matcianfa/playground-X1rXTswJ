6276fb4ba1b46f02f41537fc2bf1c87239a75d87
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-27 18:52:35 +0100

    Create Calcul_de_sommes_Test.py

---
+++ python-project/Les_boucles/Calcul_de_sommes_Test.py
@@ -0,0 +1,55 @@
+#Ne pas oublier de changer le module à importer
+from Calcul_de_sommes import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+((10,1),55),\
+((10,2),385),\
+((10,0),10),\
+((10,3),3025),\
+((1,3),1),\
+((10,-1),2.9289682539682538),\
+((10,-2),1.5497677311665408)\
+]
+
+
+#message d'aide si besoin
+help=""
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
