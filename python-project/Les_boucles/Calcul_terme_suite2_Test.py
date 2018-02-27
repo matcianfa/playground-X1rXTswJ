510825e322da1aa1d7c362a2dee73e58e738ec6f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-27 18:34:52 +0100

    Create Calcul_terme_suite2_Test.py

---
+++ python-project/Les_boucles/Calcul_terme_suite2_Test.py
@@ -0,0 +1,54 @@
+#Ne pas oublier de changer le module à importer
+from Calcul_terme_suite2 import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+((1,0,2,10),2.0),\
+((2,0,1,10),1024.0),\
+((1,5,2,10),52.0),\
+((0,3,2,10),3.0),\
+((0,3,2,0),2.0),\
+((-2,-5,-4,10),-2391.0)\
+]
+
+
+#message d'aide si besoin
+help=""
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
