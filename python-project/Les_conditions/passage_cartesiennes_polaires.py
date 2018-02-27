1e837803b0aea3dc1bc02d5358eb237e58b9cabc
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-27 12:35:47 +0100

    Create passage_cartesiennes_polaires.py

---
+++ python-project/Les_conditions/passage_cartesiennes_polaires.py
@@ -0,0 +1,6 @@
+form math import *
+
+def mon_programme(x,y):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
