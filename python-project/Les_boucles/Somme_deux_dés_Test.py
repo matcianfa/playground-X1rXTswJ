f95bb6843dcfcbe60408d607695d3977245c7c1f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-27 22:39:21 +0100

    Create Somme_deux_dés_Test.py

---
+++ python-project/Les_boucles/Somme_deux_dés_Test.py
@@ -0,0 +1,54 @@
+#Ne pas oublier de changer le module à importer
+from Somme_deux_dés import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+(2,1),\
+(4,3),\
+(6,5),\
+(8,5),\
+(12,1),\
+(1,0)\
+]
+
+
+#message d'aide si besoin
+help=""
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
