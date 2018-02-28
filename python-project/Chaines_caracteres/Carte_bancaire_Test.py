c52fe1afe52949050c0c745c50c28c0625ce7ace
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-28 22:17:35 +0100

    Create Carte_bancaire_Test.py

---
+++ python-project/Chaines_caracteres/Carte_bancaire_Test.py
@@ -0,0 +1,55 @@
+#Ne pas oublier de changer le module à importer
+from Carte_bancaire import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+("1234567891234565","NON VALIDE"),\
+("4970101234567899","NON VALIDE"),\
+("1234567891234563","VALIDE"),\
+("4970101234567893","VALIDE"),\
+("1001001001001002","VALIDE"),\
+("1234567898","NON VALIDE"),\
+("101010101010101012","NON VALIDE"),\
+]
+
+
+#message d'aide si besoin
+help="Attention, il faut qu'il y ait 16 chiffres !"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
