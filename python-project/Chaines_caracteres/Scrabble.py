f0f0172b9299c8a44a8f9d7952ca8ee170ef24aa
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-02-28 18:38:18 +0100

    Update Scrabble.py

--- python-project/Chaines_caracteres/Scrabble.py
+++ python-project/Chaines_caracteres/Scrabble.py
@@ -2,3 +2,5 @@ def mon_programme(tirage):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
