da7c165deb03564ec3d4ede8830198d845e1f803
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-01 21:29:15 +0100

    Create Codage_vigenere_Test.py

---
+++ python-project/Chaines_caracteres/Codage_vigenere_Test.py
@@ -0,0 +1,55 @@
+#Ne pas oublier de changer le module à importer
+from Codage_vigenere import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+(("ABC","ABC"),"ACE"),\
+(("C","ABCDEF"),"CDEFGH"),\
+(("BCD","ZZZZZ"),"ABCAB"),\
+(("MATHS","PYTHON"),"BYMOGZ"),\
+(("SINUS","COSINUS"),"UWFCFMA"),\
+(("RACINE","CARREE"),"TATZRI"),\
+(("GPLUS","DIDEES"),"JXOYWY"),\
+]
+
+
+#message d'aide si besoin
+help=""
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
