18cdb4cfca1d3cc659725b0f8991dcd504dd8717
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-03 20:13:09 +0100

    Create Triangle_de_Pascal_Test.py

---
+++ python-project/Les_listes/Triangle_de_Pascal_Test.py
@@ -0,0 +1,52 @@
+#Ne pas oublier de changer le module à importer
+from Triangle_de_Pascal import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+(1,[1,1]),\
+(3,[1,3,3,1]),\
+(5,[1,5,10,10,5,1]),\
+(12,[1,12,66,220,495,792,924,792,495,220,66,12,1])\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
