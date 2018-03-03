802a56546c6cf349f6832dd76be1456e0ba36165
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-03 21:50:44 +0100

    Update Triangle_de_Sierpinski.py

--- python-project/Les_listes/Triangle_de_Sierpinski.py
+++ python-project/Les_listes/Triangle_de_Sierpinski.py
@@ -1,3 +1,6 @@
+from Dessiner_Sierpinski import dessiner
+dimension = 256
+
 def mon_programme(n):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
