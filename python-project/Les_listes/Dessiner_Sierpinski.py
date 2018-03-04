49aead68816795bffa3dfa20f6fef16cd21bfa03
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-04 15:47:29 +0100

    Update Dessiner_Sierpinski.py

--- python-project/Les_listes/Dessiner_Sierpinski.py
+++ python-project/Les_listes/Dessiner_Sierpinski.py
@@ -13,6 +13,6 @@ def dessiner(liste):
   i=len(liste)-1
   for j,element in enumerate(liste):
     if element%2!=0: 
-      draw.point([x, y], (0,0,0))
+      draw.point([j, i], (0,0,0))
       
 im.save('output.png', 'PNG')
