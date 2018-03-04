154633bf746e50b2c441daa19a9e4bcd53a058e7
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-04 19:51:55 +0100

    Create Tri_selection_Test.py

---
+++ python-project/Les_listes/Tri_selection_Test.py
@@ -0,0 +1,53 @@
+#Ne pas oublier de changer le module à importer
+from Nb_racines_poly_second_degre import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+([1,5,6],[1,5,6]),\
+([3,2,5,7,10,1],[1,2,3,5,7,10]),\
+([10,9,8,7,6,5,4,3,2,1],[1,2,3,4,5,6,7,8,9,10]),\
+([10,9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9,10],[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]),\
+([1],[1])\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
