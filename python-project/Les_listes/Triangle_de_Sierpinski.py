8a3ef906fd24967bf5daeff230d04438eb191a96
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-04 18:07:31 +0100

    Update Triangle_de_Sierpinski.py

--- python-project/Les_listes/Triangle_de_Sierpinski.py
+++ python-project/Les_listes/Triangle_de_Sierpinski.py
@@ -1,6 +1,6 @@
-dimension=64
+#Vous pouvez modifier la dimension. Le mieux est de prendre une puissance de 2
+dimension=2**4
 
 def mon_programme(n):
     #Ne pas toucher ce qui précède
-    #Les valeurs pour les variables en entrée seront automatiquement données
-    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    #Copier-coller votre programme précédent
