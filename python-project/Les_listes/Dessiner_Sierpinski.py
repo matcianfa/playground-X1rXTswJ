7bd7424cd157ae3fa0e275150d10e844691e6a75
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-04 17:08:05 +0100

    Update Dessiner_Sierpinski.py

--- python-project/Les_listes/Dessiner_Sierpinski.py
+++ python-project/Les_listes/Dessiner_Sierpinski.py
@@ -2,17 +2,17 @@ from PIL import Image, ImageDraw
 dimension = 256
 
 # Image size (pixels)
-WIDTH = dimension
-HEIGHT = dimension
-palette = []
 
-im = Image.new('RGB', (WIDTH, HEIGHT), (255, 255, 255))
-draw = ImageDraw.Draw(im)
+
 
 def dessiner(liste):
+  WIDTH = dimension
+  HEIGHT = dimension
+  palette = []
+  im = Image.new('RGB', (WIDTH, HEIGHT), (255, 255, 255))
+  draw = ImageDraw.Draw(im)
   i=len(liste)-1
   for j,element in enumerate(liste):
     if element%2!=0: 
       draw.point([j, i], (0,0,0))
-      
-im.save('output.png', 'PNG')
+  im.save('output.png', 'PNG')
