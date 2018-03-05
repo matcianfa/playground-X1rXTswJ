5caecd9bfdd3d3a201f523fe728e2ddcc4571ffb
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-05 18:50:33 +0100

    Create liste_nombres_premiers2_Test.py

---
+++ python-project/Les_listes/liste_nombres_premiers2_Test.py
@@ -0,0 +1,52 @@
+#Ne pas oublier de changer le module à importer
+from liste_nombres_premiers2 import mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+(10,[2,3,5,7]),\
+(100,[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 et 97]),\
+(300014111**2,[]),\
+(1,[])\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
