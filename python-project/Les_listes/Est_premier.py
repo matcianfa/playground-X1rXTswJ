9f844222e80983eefdcf8f29cdbcf8b2f4f21a19
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-05 18:05:35 +0100

    Create Est_premier.py

---
+++ python-project/Les_listes/Est_premier.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+def est_premier(n):
+  if n<2 or (n%2==0 and n!=2):
+      print("PAS PREMIER")
+  else:
+      for d in range(3,int(sqrt(n))+1,2):
+          if n%d==0:
+              print("PAS PREMIER")
+              break
+      else:
+          print("PREMIER")
