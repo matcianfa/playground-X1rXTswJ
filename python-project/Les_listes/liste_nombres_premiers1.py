52ea9efff20590d7a60899a48e987b2f9dbdbb90
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-05 18:18:30 +0100

    Create liste_nombres_premiers1.py

---
+++ python-project/Les_listes/liste_nombres_premiers1.py
@@ -0,0 +1,7 @@
+from Est_premier import est_premier
+
+def mon_programme(n):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+
