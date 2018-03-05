314740d7ab9af4c83d7e6b4e062e5ecc8a91f228
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-05 18:09:36 +0100

    Update Est_premier.py

--- python-project/Les_listes/Est_premier.py
+++ python-project/Les_listes/Est_premier.py
@@ -2,11 +2,10 @@ from math import sqrt
 
 def est_premier(n):
   if n<2 or (n%2==0 and n!=2):
-      print("PAS PREMIER")
+      return False
   else:
       for d in range(3,int(sqrt(n))+1,2):
           if n%d==0:
-              print("PAS PREMIER")
-              break
+              return False
       else:
-          print("PREMIER")
+          return True
