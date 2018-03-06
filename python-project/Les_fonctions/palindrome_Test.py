6460b49c88845871e3a40c2ba7cc966b54751d99
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-06 22:17:44 +0100

    Create palindrome_Test.py

---
+++ python-project/Les_fonctions/palindrome_Test.py
@@ -0,0 +1,49 @@
+#Ne pas oublier de changer le module à importer
+from palindrome import est_palindrome as f
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+("lol",True),\
+("maths",False),\
+("radar",True),\
+("elle",True),\
+("lui",False)\
+]
+
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser return pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for x,reponse in input_output:
+        assert f(x) == reponse, "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(x),str(f(x)),str(reponse))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(x),str(f(x))))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
