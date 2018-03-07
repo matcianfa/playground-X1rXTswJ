1fe0ab99ffc31da222e0331c7a7142f3cdcc7783
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-07 20:04:36 +0100

    Update Volterra_plot.py

--- python-project/Les_fonctions/Volterra_plot.py
+++ python-project/Les_fonctions/Volterra_plot.py
@@ -6,5 +6,6 @@ from functools import lru_cache
 
 
 #Copiez collez vos fonctions u et v au dessus de ces lignes
+#Le but des lignes ci-dessous est d'accélérer énormément les calculs. Ne pas y toucher.
 u=lru_cache(maxsize=None)(u)
 v=lru_cache(maxsize=None)(v)
