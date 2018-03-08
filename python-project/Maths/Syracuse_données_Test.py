7f8114dd595d40aafe035e34ae57f18321b26621
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-08 18:58:36 +0100

    Create Syracuse_données_Test.py

---
+++ python-project/Maths/Syracuse_données_Test.py
@@ -0,0 +1,52 @@
+#Ne pas oublier de changer le module à importer
+from Syracuse_données import données as mon_programme
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+(5,"16 5"),\
+(15,"160 17"),\
+(127,"4372 46"),\
+(126,"9232 108")\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","Pour N={} l'altitude et le temps de vol sont bien {}".format(str(inp),str(count1).replace(" "," et ")))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
