cdb28568bdbd8e107ed8abea317e81752e0befd0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-09 19:55:13 +0100

    Create Juste_prix_Test.py

---
+++ python-project/Maths/Juste_prix_Test.py
@@ -0,0 +1,8 @@
+
+
+def mon_programme(liste):
+    #Ne pas toucher le nom du programme qui précède
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    #N'oubliez pas de rajouter "global" suivi du nom des variables si vous avez des variables globales
+
+    
