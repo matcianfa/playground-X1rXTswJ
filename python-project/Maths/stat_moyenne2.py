11592bf301cb44a8f2d619a5362cebe672f0e012
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-09 09:53:40 +0100

    Create stat_moyenne2.py

---
+++ python-project/Maths/stat_moyenne2.py
@@ -0,0 +1,6 @@
+def mon_programme(liste_valeurs,liste_pondérations):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
