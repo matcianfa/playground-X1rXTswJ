2db237d09f08419ce0e1589e33b9bc31a16944d0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-09 20:11:38 +0100

    Update Juste_prix_Test.py

--- python-project/Maths/Juste_prix_Test.py
+++ python-project/Maths/Juste_prix_Test.py
@@ -1,8 +1,42 @@
+#Ne pas oublier de changer le module à importer
+from Juste_prix import mon_programme
+from random import randint
+import sys
+import io
 
 
-def mon_programme(liste):
-    #Ne pas toucher le nom du programme qui précède
-    #Ecrire ci-dessous en n'oubliant pas d'indenter
-    #N'oubliez pas de rajouter "global" suivi du nom des variables si vous avez des variables globales
 
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser return pour afficher le resultat qui doit être un entier"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Le juste prix !","Bravo ! C'était bien le juste prix !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
     
+
+def test():
+    nombre_a_trouver=randint(1,2000)
+    n_joueur=mon_programme("Go")
+    while 1:
+        send_msg("Le juste prix !", "Vous proposez "+str(n_joueur))
+        if n_joueur==nombre_a_trouver:
+            success()
+            break
+        elif n_joueur>nombre_a_trouver:
+            n_joueur=mon_programme("Plus petit")
+        else:
+            n_joueur=mon_programme("Plus grand")
+
+
+if __name__ == "__main__": test()
