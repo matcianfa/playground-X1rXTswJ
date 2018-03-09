07fd5003f065219a21e99845a5180d42b08306d3
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-09 20:31:34 +0100

    Update Juste_prix_Test.py

--- python-project/Maths/Juste_prix_Test.py
+++ python-project/Maths/Juste_prix_Test.py
@@ -4,6 +4,22 @@ from random import randint
 import sys
 import io
 
+#Mon IA
+prix_minIA=0
+prix_maxIA=2000
+mon_prixIA=1000
+
+def mon_programmeIA(paroles):
+    global prix_minIA, prix_maxIA, mon_prixIA
+    if paroles=="Go": return mon_prixIA
+    elif paroles=="Plus grand":
+        prix_minIA=mon_prixIA
+        mon_prixIA=(prix_minIA+prix_maxIA)//2
+        return mon_prixIA
+    else:
+        prix_maxIA=mon_prixIA
+        mon_prixIA=(prix_minIA+prix_maxIA)//2
+        return mon_prixIA
 
 
 
@@ -28,15 +44,31 @@ def fail():
 def test():
     nombre_a_trouver=randint(1,2000)
     n_joueur=mon_programme("Go")
+    n_IA=mon_programmeIA("Go")
+    compteur=0
     while 1:
+        compteur+=1
+        if compteur==8: send_msg("Le juste prix !", "Olala quel suspens !")
         send_msg("Le juste prix !", "Vous proposez "+str(n_joueur))
         if n_joueur==nombre_a_trouver:
             success()
             break
         elif n_joueur>nombre_a_trouver:
+            send_msg("Le juste prix !", "Le présentateur vous dit plus petit !")
             n_joueur=mon_programme("Plus petit")
         else:
+            send_msg("Le juste prix !", "Le présentateur vous dit plus grand !")
             n_joueur=mon_programme("Plus grand")
-
-
+        send_msg("Le juste prix !", "Le présentateur se tourne vers votre adversaire !")
+        send_msg("Le juste prix !", "Votre adversaire propose "+str(n_IA))
+        if n_IA==nombre_a_trouver:
+            send_msg("Le juste prix !", "Que c'est dommage ! votre adversaire vient de trouver le juste prix de "+str(nombre_a_trouver))
+            fail()
+            break
+        elif n_IA>nombre_a_trouver:
+            send_msg("Le juste prix !", "Le présentateur vous dit plus petit !")
+            n_IA=mon_programmeIA("Plus petit")
+        else:
+            send_msg("Le juste prix !", "Le présentateur vous dit plus grand !")
+            n_IA=mon_programmeIA("Plus grand")
 if __name__ == "__main__": test()
