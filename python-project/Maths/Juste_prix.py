888e20e9d30a787324cbcbacd2d4a0aad3a5c747
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-09 19:57:40 +0100

    Update Juste_prix.py

--- python-project/Maths/Juste_prix.py
+++ python-project/Maths/Juste_prix.py
@@ -1,6 +1,6 @@
 
 
-def mon_programme(liste):
+def mon_programme(paroles):
     #Ne pas toucher le nom du programme qui précède
     #Ecrire ci-dessous en n'oubliant pas d'indenter
     #N'oubliez pas de rajouter "global" suivi du nom des variables si vous avez des variables globales
