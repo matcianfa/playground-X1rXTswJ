c341c15b2c52145fc3b8d02716999ed53b6bb2c2
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-11 19:52:44 +0100

    Create Monte_carlo_plot.py

---
+++ python-project/Maths/Monte_carlo_plot.py
@@ -0,0 +1,12 @@
+from random import uniform
+from Trouver_max_fonction import maximum
+
+#Les paramètres que vous pouvez modifier pour visualiser
+f="sqrt(1-x**2)"
+a=0
+b=1
+nombre_de_points_affichés=500
+
+def mon_programme(f,a,b):
+    #Copiez collez votre programme précédent ci-dessous pour le modifier
+    
