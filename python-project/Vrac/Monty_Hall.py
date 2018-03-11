8826b4b2503a04ecfe34f1836752ce596cc6bb2f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-11 10:05:13 +0100

    Create Monty_Hall.py

---
+++ python-project/Vrac/Monty_Hall.py
@@ -0,0 +1,8 @@
+from random import randint
+
+
+def mon_programme(numéro):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    #Si vous utilisez des variables globales, n'oubliez pas d'ajouter en premier ci dessous 'global' suivi des noms des variables séparés par des virgules.
