7b1e61de390288fe3e306f99052474e7e3f04552
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-11 18:13:42 +0100

    Create Monte_carlo.py

---
+++ python-project/Maths/Monte_carlo.py
@@ -0,0 +1,10 @@
+from random import uniform
+from Trouver_max_fonction import maximum
+
+def mon_programme(f,a,b):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
