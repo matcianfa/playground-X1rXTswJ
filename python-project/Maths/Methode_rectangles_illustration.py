d6a7a5bb532b28d584674c54da5d88d651a3e1da
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-12 22:33:04 +0100

    Update Methode_rectangles_illustration.py

--- python-project/Maths/Methode_rectangles_illustration.py
+++ python-project/Maths/Methode_rectangles_illustration.py
@@ -2,6 +2,7 @@
 #Attention la fonction doit rester entre guillemets 
 #et être un calcul compréhensible par Python (par exemple ** pour les puissances)
 #Les fonctions cos, sin, tan, exp ln etc. du module math sont utilisables sans rien devant.
+#Vous pouvez par exemple tester avec f="x*(1-x)*(sin(200*x*(1-x)))**2" entre 0 et 1 pour voir que si on prend un n trop petit la méthode n'est pas très efficace
 
 f="2*x**3-3*x**2+2"
 a=-0.5
