1f5b572284783c09dcf71a9dbacdd45bef4501da
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-12 20:29:29 +0100

    Create Methode_rectangles_illustration.py

---
+++ python-project/Maths/Methode_rectangles_illustration.py
@@ -0,0 +1,9 @@
+# Paramètres modifiables. 
+#Attention la fonction doit rester entre guillemets 
+#et être un calcul compréhensible par Python (par exemple ** pour les puissances)
+#Les fonctions cos, sin, tan, exp ln etc. du module math sont utilisables sans rien devant.
+
+f="2*x**3-3*x**2+2"
+a=-0.5
+b=1.5
+n=20
