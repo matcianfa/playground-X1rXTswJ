0494e0e185c42be054e22d2e3540439546b1fede
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-13 22:17:29 +0100

    Create Tracer_une_courbe.py

---
+++ python-project/Les_listes/Tracer_une_courbe.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+def mon_programme(f,a,b,n):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+
