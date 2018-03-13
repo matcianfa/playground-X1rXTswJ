0224d6e5ab3a6b068e334931bc65c9a4a234cbcc
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-13 23:36:11 +0100

    Update Tracer_une_courbe_Test.py

--- python-project/Les_listes/Tracer_une_courbe_Test.py
+++ python-project/Les_listes/Tracer_une_courbe_Test.py
@@ -41,7 +41,7 @@ def send_msg(channel, msg):
 
 
 def success():
-    print("TECHIO> open -s /project/target/ index.html")
+    print("TECHIO> open -s /project/target/ index_grossissant.html")
     print("TECHIO> success true")
 
 
