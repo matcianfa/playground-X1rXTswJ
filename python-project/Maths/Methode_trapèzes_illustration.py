8841e6c6f3ee630f882b2870527f132d6ccbd69e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-13 18:45:54 +0100

    Create Methode_trapèzes_illustration.py

---
+++ python-project/Maths/Methode_trapèzes_illustration.py
@@ -0,0 +1,10 @@
+# Paramètres modifiables. 
+#Attention la fonction doit rester entre guillemets 
+#et être un calcul compréhensible par Python (par exemple ** pour les puissances)
+#Les fonctions cos, sin, tan, exp ln etc. du module math sont utilisables sans rien devant.
+#Vous pouvez par exemple tester avec f="x*(1-x)*(sin(200*x*(1-x)))**2" entre 0 et 1 pour voir que si on prend un n trop petit la méthode n'est pas très efficace
+
+f="2*x**3-3*x**2+2"
+a=-0.5
+b=1.5
+n=20
