212cda76595e93ad93124673812a73c90718aead
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-16 16:46:24 +0100

    Update courbe_dragon.py

--- python-project/Vrac/courbe_dragon.py
+++ python-project/Vrac/courbe_dragon.py
@@ -1,4 +1,4 @@
-def mon_programme(k):
+def pliage(k):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
