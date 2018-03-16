e3141db721abcb9f51647bd4503e66e0052862e7
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-16 21:45:23 +0100

    Update courbe_dragon_plot_Test.py

--- python-project/Vrac/courbe_dragon_plot_Test.py
+++ python-project/Vrac/courbe_dragon_plot_Test.py
@@ -44,6 +44,7 @@ def test():
         res=mon_programme(n)
         assert rep==res , "Les coordonnées obtenues pour n={} sont {} au lieu de {}".format(str(n),str(res),str(rep))
         send_msg("Résultats validés", "Les coordonnées du dernier point pour n ={} sont bien {}".format(str(n),str(res)))
+      fig=plt.figure()
       mon_programme(n_dessin)
       plt.axis('equal')
       plt.axis('off')
