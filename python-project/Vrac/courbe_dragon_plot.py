a8d5ff38ddaddf38512d3f4b6ac4b119757c1a91
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-16 20:19:01 +0100

    Create courbe_dragon_plot.py

---
+++ python-project/Vrac/courbe_dragon_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+#Rajouter ici votre fonction pliage précédente
+
+
+
+#Compléter la fonction ci-dessous pour qu'elle affiche la courbe du dragon
+def mon_programme(n):
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
