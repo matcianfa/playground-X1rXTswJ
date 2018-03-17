f3f66298e5ef3df08ecf146112f3ffa7b7128018
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-17 22:50:58 +0100

    Create bac_a_sable_Test.py

---
+++ python-project/bac_a_sable_Test.py
@@ -0,0 +1,47 @@
+#Ne pas oublier de changer le module à importer
+from bac_a_sable import *
+import sys
+import io
+
+
+#liste des couples input/output
+input_output=[\
+((1,5,6),2),\
+((1,1,8),0),\
+((1,-2,1),1),\
+((0.25,1.25,1.5),2)\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print pour afficher le resultat"
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    print("TECHIO> open -s /project/target/ index.html")
+    
+    send_msg("Tests validés","Bravo !")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      print("TECHIO> open -s /project/target/ universe.py")
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
