0c417bfce0f85bf53413495593df72d98fcf6740
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-17 11:25:24 +0100

    Create tri_stupide.py

---
+++ python-project/Vrac/tri_stupide.py
@@ -0,0 +1,11 @@
+from random import randint
+
+#Copiez collez ci-dessous vos fonctions est_triée et mélanger
+
+
+
+
+def mon_programme(liste):
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
