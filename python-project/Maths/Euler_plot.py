844416897c4acaaa4ae648e143da35e4611b25ef
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-17 18:49:35 +0100

    Update Euler_plot.py

--- python-project/Maths/Euler_plot.py
+++ python-project/Maths/Euler_plot.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
+#Paramètres modifiables pour le tracé final.
+#pas_negatif permet de tracer la courbe pour les x négatifs. Pour ne pas faire apparaitre cette partie, il suffit de mettre 0.
+pas=0.1
+pas_negatif=-pas
+n=20
+
 def mon_programme(pas,n):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
