e4ff7af5f8f0af6edb77f829ac5b38aba104d304
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-17 10:58:10 +0100

    Create melanger.py

---
+++ python-project/Vrac/melanger.py
@@ -0,0 +1,9 @@
+from random import randint
+
+def mélanger(liste):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
