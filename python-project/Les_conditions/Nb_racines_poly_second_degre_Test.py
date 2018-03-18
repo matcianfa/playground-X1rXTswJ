799934e6620a4101a189752ab308d35ae463a8a4
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:53:43 +0100

    Update Nb_racines_poly_second_degre_Test.py

--- python-project/Les_conditions/Nb_racines_poly_second_degre_Test.py
+++ python-project/Les_conditions/Nb_racines_poly_second_degre_Test.py
@@ -1,4 +1,5 @@
 #Ne pas oublier de changer le module à importer
+module="Nb_racines_poly_second_degre"
 from Nb_racines_poly_second_degre import mon_programme
 import sys
 import io
@@ -16,6 +17,19 @@ input_output=[\
 #message d'aide si besoin
 help="N'oublie pas d'utiliser print pour afficher le resultat"
 
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+    pass
+
 
 
 def send_msg(channel, msg):
@@ -24,6 +38,7 @@ def send_msg(channel, msg):
 
 def success():
     send_msg("Tests validés","Bravo !")
+    afficher_correction()
     print("TECHIO> success true")
 
 
