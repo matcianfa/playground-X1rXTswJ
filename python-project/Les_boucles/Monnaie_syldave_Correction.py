d0f87b9506f6c84dece68e8b902ec767c1258980
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 19:10:00 +0100

    Create Monnaie_syldave_Correction.py

---
+++ python-project/Les_boucles/Monnaie_syldave_Correction.py
@@ -0,0 +1,9 @@
+def mon_programme(k):
+    somme=601
+    for a in range(-10,10):
+        for b in range(-10,10):
+            for c in range(-10,10):
+                if a*57+b*62+c*72==k and max(0,a)*57+max(0,b)*62+max(0,c)*72<somme:
+                    somme=max(0,a)*57+max(0,b)*62+max(0,c)*72
+    if somme<601:print(somme)
+    else :print("Impossible")
