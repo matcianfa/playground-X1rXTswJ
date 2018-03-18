39d2e9968299cb0a949662f208f100e2bd7d18d8
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:49:27 +0100

    Create passage_cartesiennes_polaires_Correction.py

---
+++ python-project/Les_conditions/passage_cartesiennes_polaires_Correction.py
@@ -0,0 +1,8 @@
+def mon_programme(x,y):
+    if y!=0 or x>0:
+        r=round(sqrt(x**2+y**2),3)
+        theta=round(2*atan(y/(x+r)),3)
+    else :
+        r=-x
+        theta=round(pi,3)
+    print(r,theta)
