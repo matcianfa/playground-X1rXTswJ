e585b8c395faa15a1a3b7995dbb2a94982772564
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:59:56 +0100

    Update passage_cartesiennes_polaires_Test.py

--- python-project/Les_conditions/passage_cartesiennes_polaires_Test.py
+++ python-project/Les_conditions/passage_cartesiennes_polaires_Test.py
@@ -1,4 +1,5 @@
 #Ne pas oublier de changer le module à importer
+module="passage_cartesiennes_polaires"
 from passage_cartesiennes_polaires import mon_programme
 import sys
 import io
@@ -19,6 +20,19 @@ input_output=[\
 #message d'aide si besoin
 help="N'oublie pas d'utiliser print pour afficher le resultat"
 
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+    pass
+
 
 
 def send_msg(channel, msg):
@@ -27,6 +41,7 @@ def send_msg(channel, msg):
 
 def success():
     send_msg("Tests validés","Bravo !")
+    afficher_correction()
     print("TECHIO> success true")
 
 
