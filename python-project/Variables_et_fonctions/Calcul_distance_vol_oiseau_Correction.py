f0f5940d496dfc8dd2150941113d13c8f6aa7d5d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 18:29:19 +0100

    Create Calcul_distance_vol_oiseau_Correction.py

---
+++ python-project/Variables_et_fonctions/Calcul_distance_vol_oiseau_Correction.py
@@ -0,0 +1,6 @@
+def mon_programme(lat_a,long_a,lat_b,long_b):
+    rayon=6371.
+    x=(long_b-long_a)*pi/180*cos((lat_a+lat_b)*pi/360)
+    y=(lat_b-lat_a)*pi/180
+    distance = rayon*sqrt(x**2+y**2)
+    print(round(distance,3))
