4fc8160b23faf25619127f2788f4cd9e3c143a1d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:56:06 +0100

    Update Triangle_rectangle_0_Test.py

--- python-project/Les_conditions/Triangle_rectangle_0_Test.py
+++ python-project/Les_conditions/Triangle_rectangle_0_Test.py
@@ -1,4 +1,5 @@
 #Ne pas oublier de changer le module à importer
+module="Triangle_rectangle_0"
 from Triangle_rectangle_0 import mon_programme
 import sys
 import io
@@ -17,6 +18,19 @@ input_output=[\
 #message d'aide si besoin
 help="L'angle qui est droit n'est pas forcément le premier ou le dernier..."
 
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+    pass
+
 
 
 def send_msg(channel, msg):
@@ -25,6 +39,7 @@ def send_msg(channel, msg):
 
 def success():
     send_msg("Tests validés","Bravo !")
+    afficher_correction()
     print("TECHIO> success true")
 
 
