7bba6679bb59454ca0302da01869c0a5e9d840a0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 19:04:52 +0100

    Create  	Recherche_de_seuil2_Correction.py

---
+++ python-project/Les_boucles/Recherche_de_seuil2_Correction.py
@@ -0,0 +1,9 @@
+def mon_programme(u_0,e):
+    if u_0>e: print(0)
+    elif u_0<=0 : print("IMPOSSIBLE")
+    else:
+        n=0
+        while u_0<e:
+            n+=1
+            u_0*=2
+        print(n)
