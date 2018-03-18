0f5c958d34377d3e259b6209a66dd824e1417159
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 22:02:27 +0100

    Update Champernowne_Correction.py

--- python-project/Les_boucles/Champernowne_Correction.py
+++ python-project/Les_boucles/Champernowne_Correction.py
@@ -1,6 +1,6 @@
 def mon_programme(n):
-    print("0",end="")
+    print('0',end='')
     if n>0:
-        print(".",end="")
+        print('.',end='')
         for i in range(1,n+1):
-            print(i,end="")
+            print(i,end='')
