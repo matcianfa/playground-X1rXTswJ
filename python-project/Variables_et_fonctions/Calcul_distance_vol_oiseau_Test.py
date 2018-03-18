64e317e9272d7df5103688f0a4d16b0aabcf759e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 18:40:37 +0100

    Update Calcul_distance_vol_oiseau_Test.py

--- python-project/Variables_et_fonctions/Calcul_distance_vol_oiseau_Test.py
+++ python-project/Variables_et_fonctions/Calcul_distance_vol_oiseau_Test.py
@@ -1,3 +1,4 @@
+module="Calcul_distance_vol_oiseau"
 from Calcul_distance_vol_oiseau import mon_programme
 import sys
 import io
@@ -19,6 +20,19 @@ input_output=[\
 #message d'aide si besoin
 help="N'oublie pas d'utiliser print pour afficher le resultat"
 
+#Afficher la correction
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+        pass
 
 
 def send_msg(channel, msg):
@@ -27,6 +41,7 @@ def send_msg(channel, msg):
 
 def success():
     send_msg("Tests validés","Bravo !")
+    afficher_correction()
     print("TECHIO> success true")
 
 
