9ac707216126bd0cd128f449e50accdf5398a937
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:52:37 +0100

    Update Nature_d_un_triangle_Test.py

--- python-project/Les_conditions/Nature_d_un_triangle_Test.py
+++ python-project/Les_conditions/Nature_d_un_triangle_Test.py
@@ -1,4 +1,5 @@
 #Ne pas oublier de changer le module à importer
+module="Nature_d_un_triangle"
 from Nature_d_un_triangle import mon_programme
 import sys
 import io
@@ -22,7 +23,19 @@ input_output=[\
 #message d'aide si besoin
 help="N'oublie pas qu'être rectangle ou isocèle doit se vérifier aux 3 sommets."
 
-
+#Afficher la correction
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+    pass
 
 def send_msg(channel, msg):
     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
@@ -30,6 +43,7 @@ def send_msg(channel, msg):
 
 def success():
     send_msg("Tests validés","Bravo !")
+    afficher_correction()
     print("TECHIO> success true")
 
 
