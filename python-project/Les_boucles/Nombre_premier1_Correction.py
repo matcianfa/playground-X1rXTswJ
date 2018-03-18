0563c72c40d5c44b326cf8d0963c47802bcdfa2a
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 22:07:36 +0100

    Update Nombre_premier1_Correction.py

--- python-project/Les_boucles/Nombre_premier1_Correction.py
+++ python-project/Les_boucles/Nombre_premier1_Correction.py
@@ -1,10 +1,10 @@
 def mon_programme(n):
     if n<2:
-        print("PAS PREMIER")
+        print('PAS PREMIER')
     else:
         for d in range(2,n):
             if n%d==0:
-                print("PAS PREMIER")
+                print('PAS PREMIER')
                 break
         else:
-            print("PREMIER")
+            print('PREMIER')
