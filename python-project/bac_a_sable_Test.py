ab10822154b85f47d676f93912e143ed321723b8
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 09:59:09 +0100

    Update bac_a_sable_Test.py

--- python-project/bac_a_sable_Test.py
+++ python-project/bac_a_sable_Test.py
@@ -19,7 +19,7 @@ help="N'oublie pas d'utiliser print pour afficher le resultat"
 
 #Afficher la correction
 def afficher_correction():
-    with open(module+"_Correction.txt", "r") as correction
+    with open(module+"_Correction.txt", "r") as correction :
         ligne="Voici un ou des exemples de corrections possibles"
         send_msg("Exemple(s) de correction", ligne)
         ligne="-------------------------------------------------"
