cc5c812c25e33a407fc22409b85ec7359a209f4e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:47:54 +0100

    Create Triangles_constructibles_Correction.py

---
+++ python-project/Les_conditions/Triangles_constructibles_Correction.py
@@ -0,0 +1,7 @@
+def mon_programme(a,b,c):
+    if a+b>=c and a+c>=b and b+c>=a:
+        print("CONSTRUCTIBLE")
+    elif a+b==c or a+c==b or b+c==a:
+        print("PLAT")
+    else:
+        print("PAS CONSTRUCTIBLE")
