6cc50d050445316ce8c6d07522393cd807e85215
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:52:54 +0100

    Update Conversion_Celsius_Fahrenheit_Test.py

--- python-project/Les_conditions/Conversion_Celsius_Fahrenheit_Test.py
+++ python-project/Les_conditions/Conversion_Celsius_Fahrenheit_Test.py
@@ -31,7 +31,7 @@ def afficher_correction():
             for ligne in lignes:
                 send_msg("Exemple(s) de correction", ligne)
     except:
-pass
+    pass
 
 def send_msg(channel, msg):
     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
