ccb8c56ad24cb69d72bda326868e482546e0fdee
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 20:33:32 +0100

    Update Triangle_rectangle_1_Correction.py

--- python-project/Les_conditions/Triangle_rectangle_1_Correction.py
+++ python-project/Les_conditions/Triangle_rectangle_1_Correction.py
@@ -1,5 +1,5 @@
 def mon_programme(a,b,c):
     if a**2-b**2-c**2==0 or b**2-a**2-c**2==0 or c**2-a**2-b**2==0:
-        print("RECTANGLE")
+        print('RECTANGLE')
     else :
-        print("PAS RECTANGLE")
+        print('PAS RECTANGLE')
