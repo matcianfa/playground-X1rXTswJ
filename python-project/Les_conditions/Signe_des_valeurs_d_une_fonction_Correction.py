d277ca9fe4c4901c158164f30d8d1112805e03e5
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-18 17:39:58 +0100

    Create Signe_des_valeurs_d_une_fonction_Correction.py

---
+++ python-project/Les_conditions/Signe_des_valeurs_d_une_fonction_Correction.py
@@ -0,0 +1,8 @@
+def mon_programme(x):
+    p=x**2-x-6
+    if p>0:
+        print("P(x) strictement positif")
+    elif p<0:
+        print("P(x) strictement négatif")
+    else :
+        print("P(x) vaut 0")
