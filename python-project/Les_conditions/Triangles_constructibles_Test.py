6660b6a652b0e2bf69d827ac44a04a28100cce1b
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:45:16 +0100

    Update Triangles_constructibles_Test.py

--- python-project/Les_conditions/Triangles_constructibles_Test.py
+++ python-project/Les_conditions/Triangles_constructibles_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_conditions/Triangles_constructibles"
-from Triangles_constructibles import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Triangles_constructibles import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
