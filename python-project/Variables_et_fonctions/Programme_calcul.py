18e4539a8b8dc3cef28c025c4dcd7f9b42e92618
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 10:03:57 +0100

    Create Programme_calcul.py

---
+++ python-project/Variables_et_fonctions/Programme_calcul.py
@@ -0,0 +1,5 @@
+def mon_programme(n):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour la variable en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+
