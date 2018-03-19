7c8f8c025a2f7f5332b94ec621ec93676653a429
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:52:07 +0100

    Update Somme_deux_dés_Test.py

--- python-project/Les_boucles/Somme_deux_dés_Test.py
+++ python-project/Les_boucles/Somme_deux_dés_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_boucles/Somme_deux_dés"
-from Somme_deux_dés import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Somme_deux_dés import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
