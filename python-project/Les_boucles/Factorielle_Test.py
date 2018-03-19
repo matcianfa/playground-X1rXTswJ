bb00636d1ba2addf7afbf5487833b6144be66f03
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:49:33 +0100

    Update Factorielle_Test.py

--- python-project/Les_boucles/Factorielle_Test.py
+++ python-project/Les_boucles/Factorielle_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_boucles/Factorielle"
-from Factorielle import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Factorielle import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
