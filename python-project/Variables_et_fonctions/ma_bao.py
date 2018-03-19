f2cad436f116293a4cf4a75218a310d6e881f0c7
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 21:54:29 +0100

    Update ma_bao.py

--- python-project/Variables_et_fonctions/ma_bao.py
+++ python-project/Variables_et_fonctions/ma_bao.py
@@ -8,13 +8,14 @@ def tester_import(txt,glb):
         return exec(txt,glb)
     except IndentationError as e:
         print("Attention il y a un problème d'indentation dans votre code à la ligne "+str(e.args[1][1]),file=sys.stderr)
-        print("Vérifiez bien que vous avez correctement décaler votre code",file=sys.stderr)
+        print("Vérifiez bien que vous avez correctement décalé votre code",file=sys.stderr)
         print("Après un if, else, elif, for, while ou la création d'une fonction, il faut ajouter un décalage.",file=sys.stderr)
         print("TECHIO> success false")
         sys.exit()
     except SyntaxError as e:
         print("Attention , il y a un problème de syntaxe dans votre code à la ligne "+str(e.args[1][1]),file=sys.stderr)
         print("Vérifiez que vous n'avez pas oublié des deux points ':' en fin de ligne par exemple.",file=sys.stderr)
+        print("Cela peut provenir d'une parenthèse mal fermée ou une virgule mal placée.",file=sys.stderr)
         print("TECHIO> success false")
         sys.exit()
     except NameError as e:
