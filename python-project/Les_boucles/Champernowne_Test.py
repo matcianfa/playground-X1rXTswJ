ced1043fdb3b0b12f34853a6f183fd5b83d4592f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:48:53 +0100

    Update Champernowne_Test.py

--- python-project/Les_boucles/Champernowne_Test.py
+++ python-project/Les_boucles/Champernowne_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_boucles/Champernowne"
-from Champernowne import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Champernowne import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
