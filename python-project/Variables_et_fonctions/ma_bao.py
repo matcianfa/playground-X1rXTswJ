a45410717d08e63a329e83d22f7b1e5826e03507
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:07:02 +0100

    Update ma_bao.py

--- python-project/Variables_et_fonctions/ma_bao.py
+++ python-project/Variables_et_fonctions/ma_bao.py
@@ -1,6 +1,6 @@
 import sys
 
-def tester_import(txt,glb):
+def importer(txt,glb):
     '''
     Permet de tester si le module n'a pas d'erreur de syntaxe ou d'indentation pour pouvoir les rattraper.
     '''
@@ -11,14 +11,14 @@ def tester_import(txt,glb):
         print("Attention il y a un problème d'indentation dans votre code à la ligne "+str(e.args[1][1]),file=sys.stderr)
         print("Vérifiez bien que vous avez correctement décalé votre code",file=sys.stderr)
         print("Après un if, else, elif, for, while ou la création d'une fonction, il faut ajouter un décalage.",file=sys.stderr)
-        print('TECHIO> annotate --file '+ nom_fichier +' --position '+str(e.args[1][1])) 
+        print('TECHIO> annotate --file '+ nom_fichier +' --position '+str(e.args[1][1])+' Erreur d\'indentation') 
         print("TECHIO> success false")
         sys.exit()
     except SyntaxError as e:
         print("Attention , il y a un problème de syntaxe dans votre code à la ligne "+str(e.args[1][1]),file=sys.stderr)
         print("Vérifiez que vous n'avez pas oublié des deux points ':' en fin de ligne par exemple.",file=sys.stderr)
         print("Cela peut provenir d'une parenthèse mal fermée ou une virgule mal placée.",file=sys.stderr)
-        print('TECHIO> annotate --file '+ nom_fichier +' --position '+str(e.args[1][1])) 
+        print('TECHIO> annotate --file '+ nom_fichier +' --position '+str(e.args[1][1]+' Erreur de syntaxe')) 
         print("TECHIO> success false")
         sys.exit()
     except NameError as e:
