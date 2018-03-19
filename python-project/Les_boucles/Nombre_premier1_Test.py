68ba13071e3c73b66e88b4b962ba400ded49d7d8
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:50:32 +0100

    Update Nombre_premier1_Test.py

--- python-project/Les_boucles/Nombre_premier1_Test.py
+++ python-project/Les_boucles/Nombre_premier1_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_boucles/Nombre_premier1"
-from Nombre_premier1 import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Nombre_premier1 import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
