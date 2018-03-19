796c50d843b013f07552276daeea61481d8877ef
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:48:34 +0100

    Update Calcul_terme_suite2_Test.py

--- python-project/Les_boucles/Calcul_terme_suite2_Test.py
+++ python-project/Les_boucles/Calcul_terme_suite2_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_boucles/Calcul_terme_suite2"
-from Calcul_terme_suite2 import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Calcul_terme_suite2 import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
