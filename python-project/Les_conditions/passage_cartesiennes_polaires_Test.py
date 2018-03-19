a8c522fa5a929708d3523aa6a1ea7a58da53c8fc
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:46:01 +0100

    Update passage_cartesiennes_polaires_Test.py

--- python-project/Les_conditions/passage_cartesiennes_polaires_Test.py
+++ python-project/Les_conditions/passage_cartesiennes_polaires_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_conditions/passage_cartesiennes_polaires"
-from passage_cartesiennes_polaires import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from passage_cartesiennes_polaires import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
