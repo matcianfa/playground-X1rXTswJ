78871bbe916fb7fff2bfe03705c34644a61cad5e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-19 22:51:08 +0100

    Update Nombres_parfaits_Test.py

--- python-project/Les_boucles/Nombres_parfaits_Test.py
+++ python-project/Les_boucles/Nombres_parfaits_Test.py
@@ -1,9 +1,10 @@
 #Ne pas oublier de changer le module à importer
 module="Les_boucles/Nombres_parfaits"
-from Nombres_parfaits import mon_programme
+
 import sys
 import io
-
+from ma_bao import *
+tester("from Nombres_parfaits import mon_programme",globals())
 
 #liste des couples input/output
 input_output=[\
