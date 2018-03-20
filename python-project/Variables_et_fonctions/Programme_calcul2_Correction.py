08e66347f7dec005f28342ce7b6a317e737fe297
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-20 08:49:08 +0100

    Create Programme_calcul2_Correction.py

---
+++ python-project/Variables_et_fonctions/Programme_calcul2_Correction.py
@@ -0,0 +1,14 @@
+from math import *
+def mon_programme(n):
+    a=n+pi
+    print(a)
+    b=sqrt(a)
+    print(b)
+    c=round(b,5)
+    print(c)
+    d=cos(a/2)
+    print(d)
+    e=round(c,5)
+    print(e)
+    f=a+c+e
+    print(f)
