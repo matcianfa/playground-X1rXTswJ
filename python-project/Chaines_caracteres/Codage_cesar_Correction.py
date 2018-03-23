79cfe008f08166d1fc01c553880a0a53c7c94e3b
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-23 18:41:53 +0100

    Create Codage_cesar_Correction.py

---
+++ python-project/Chaines_caracteres/Codage_cesar_Correction.py
@@ -0,0 +1,9 @@
+def mon_programme(n,message):
+    message_codé=''
+    for lettre in message:
+        if 65<=ord(lettre)<=90: #Si c'est une lettre, je la décale de n en utilisant l'astuce du reste de la division euclidienne
+            message_codé+=chr((ord(lettre)-65+n)%26 + 65)
+        else:
+            message_codé+=lettre
+
+    print(message_codé)
