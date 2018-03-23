b4a32cf852246fab4f53cc45491c3f5e132db84f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-23 19:24:44 +0100

    Update Euler_1_Test.py

--- python-project/Defis/Euler_1_Test.py
+++ python-project/Defis/Euler_1_Test.py
@@ -2,13 +2,13 @@
 module="Defis/Euler_1"
 import sys
 import io
+#On récupère les données de l'utilisateur
 sauvegarde_stdout=sys.stdout
 sys.stdout=io.StringIO()
-#mon_programme(*inp)
 from Euler_1 import *
 count1 = sys.stdout.getvalue()[:-1]
 sys.stdout=sauvegarde_stdout
-
+from ma_bao import *
 
 
 
@@ -18,21 +18,6 @@ reponse=233168
 
 #message d'aide si besoin
 help="N'oublie pas d'utiliser print pour afficher le resultat"
-
-def afficher_correction():
-    try:
-        with open(module+"_Correction.py", "r") as correction :
-            ligne="Voici un ou des exemples de corrections possibles"
-            send_msg("Exemple(s) de correction", ligne)
-            ligne="-------------------------------------------------"
-            send_msg("Exemple(s) de correction", ligne)
-            lignes=correction.read().split("\n")
-            for ligne in lignes:
-                send_msg("Exemple(s) de correction", ligne)
-    except:
-        pass
-
-
 
 def send_msg(channel, msg):
     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
@@ -50,13 +35,6 @@ def fail():
 
 def test():
     try:
-      '''
-      sauvegarde_stdout=sys.stdout
-      sys.stdout=io.StringIO()
-      #mon_programme(*inp)
-      from Euler_1 import *
-      count1 = sys.stdout.getvalue()[:-1]
-      sys.stdout=sauvegarde_stdout'''
       assert str(count1) == str(reponse), "Le résultat obtenu est {} mais ce n'est pas le bon.".format(str(count1))
       send_msg("Tests validés","Le résultat cherché est bien {}".format(str(count1)))
       success()
