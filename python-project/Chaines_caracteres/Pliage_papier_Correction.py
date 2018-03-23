e9a9a92dc66ecdcc83e946ef66adbf3642ed99ae
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-23 18:49:06 +0100

    Create Pliage_papier_Correction.py

---
+++ python-project/Chaines_caracteres/Pliage_papier_Correction.py
@@ -0,0 +1,20 @@
+def mon_programme(n):
+    n=n-1
+    mot="G"
+    while n>0:
+        nouveau_mot=""
+        mot+="G"
+        while mot!="":
+            début=mot[:2]
+            if début=="GG":
+                nouveau_mot+="GGDG"
+            elif début=="DG":
+                nouveau_mot+="GDDG"
+            elif début=="GD":
+                nouveau_mot+="GGDD"
+            else:
+                nouveau_mot+="GDDD"
+            mot=mot[2:]
+        mot=nouveau_mot[:-1]
+        n-=1
+    print(mot)
