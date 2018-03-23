cca166124d2f649eef2c01262f81a869f5342250
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-23 22:16:50 +0100

    Update ma_bao.py

--- python-project/ma_bao.py
+++ python-project/ma_bao.py
@@ -34,7 +34,7 @@ def send_msg(channel, msg):
     print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
     
 #Afficher la correction
-def afficher_correction():
+def afficher_correction(module):
     try:
         with open(module+"_Correction.py", "r") as correction :
             ligne="Voici un ou des exemples de corrections possibles"
