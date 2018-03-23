e76c996937225ca7c61f942247052646a8e1eca8
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-03-23 20:39:23 +0100

    Update Expression_bien_parenthesee_Test.py

--- python-project/Chaines_caracteres/Expression_bien_parenthesee_Test.py
+++ python-project/Chaines_caracteres/Expression_bien_parenthesee_Test.py
@@ -1,5 +1,5 @@
 #Ne pas oublier de changer le module à importer
-module="Les_boucles/Expression_bien_parenthesee"
+module="Chaines_caracteres/Expression_bien_parenthesee"
 
 import sys
 import io
@@ -32,7 +32,7 @@ def send_msg(channel, msg):
 
 def success():
     send_msg("Tests validés","Bravo !")
-    afficher_correction()
+    afficher_correction(module)
     print("TECHIO> success true")
 
 
