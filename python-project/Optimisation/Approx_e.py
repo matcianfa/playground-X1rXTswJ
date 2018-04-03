55463cb28b906d3c35957eb62f56180818c2f6ba
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-04-03 20:29:45 +0200

    Create Approx_e.py

---
+++ python-project/Optimisation/Approx_e.py
@@ -0,0 +1,10 @@
+
+# Créez votre fonction factorielle(n) ici. N'oubliez pas de mémoïzer.
+def factorielle(n):
+
+
+
+
+# Créez votre fonction pour calculer l'approximation de e ici.
+def mon_programme(n):
+    
