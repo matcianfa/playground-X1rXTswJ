ae0350a5cd173e5f224438f30ee244b92945c2c0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-04-03 21:27:53 +0200

    Update Approx_e.py

--- python-project/Optimisation/Approx_e.py
+++ python-project/Optimisation/Approx_e.py
@@ -1,5 +1,11 @@
+# Ne pas modifier cette ligne
+from modifier_precision import modifier_précision
+
+
 
 # Créez votre fonction factorielle(n) ici. N'oubliez pas de mémoïzer.
+# Ne pas modifier la ligne qui suit. Elle sert à augmenter la précision des calculs.
+@modifier_précision
 def factorielle(n):
 
 
