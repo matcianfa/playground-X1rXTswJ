5d08072265fa2d2fa86ae1b2bbb31c73bedb4644
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-04-10 08:47:12 +0200

    Update Euler_31_Correction.py

--- python-project/Defis/Euler_31_Correction.py
+++ python-project/Defis/Euler_31_Correction.py
@@ -3,12 +3,14 @@ pièces=[200,100,50,20,10,5,2,1]
 # décomposer(n,k) donne le nombre de façon de décomposer n avec les pièces inférieures ou égale à k
 def décomposer(n,k=200):
     somme=0
+    if n==0 or k==1: # k==1 n'est pas obligatoire mais permet de gagner beaucoup de temps : Si on en est à la pièce 1, on ne peut plus que completer avec des 1 donc il n'y a qu'une facon de le faire.
+        return 1
     for pièce in pièces:
-        if k >= pièce and n > pièce :
+        if k >= pièce and n >= pièce :
             somme+= décomposer(n-pièce, pièce)
-        elif k >= pièce and n==pièce :
-            somme+=1
     return somme
 
 print(décomposer(200))
+
+
 
