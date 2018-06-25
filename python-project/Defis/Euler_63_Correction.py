8eaddb05b15d6cf866f3dcd4301fefd2bbf49c26
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-06-25 20:00:09 +0200

    Update Euler_63_Correction.py

--- python-project/Defis/Euler_63_Correction.py
+++ python-project/Defis/Euler_63_Correction.py
@@ -1,4 +1,4 @@
-# Commençons par déterminer jusqu'à quelle valeur de n il faut chercher. en effet, pour former un nombre de n chiffres, il faut valoir moin de 10^n donc quand on cherche une puissance n-ieme, il suffit de chercher pour les nombres de 2 à 9.
+# Commençons par déterminer jusqu'à quelle valeur de n il faut chercher. en effet, pour former un nombre de n chiffres, il faut valoir moins de 10^n donc quand on cherche une puissance n-ieme, il suffit de chercher pour les nombres de 2 à 9.
 # De plus, si 9^n< 10^(n-1), le nombre obtenu n'aura que n-1 chiffres et donc il n'existera aucune puissance n-ieme de n chiffres. Cette inéquation revient à avoir ln(10)/ln(10/9)<n autrement dit, il faut n<=21 pour qu'il y ait des solutions.
 
 compteur=0
