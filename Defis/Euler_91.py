d64e34a57a5919f401d18f4025303c38bdd7007c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-07-01 16:49:42 +0200

    Create Euler_91.py

---
+++ Defis/Euler_91.py
@@ -0,0 +1,19 @@
+# Triangles rectangles de coordonnées entières
+`Difficulté : Moyen (25%)`
+`Origine : Projet Euler n°91`
+
+Les points P (x1, y1) and Q (x2, y2) ont des coordonnées entières et sont reliés à l'origine pour former le triangle OPQ.
+
+![Triangle](https://projecteuler.net/project/images/p091_1.gif)
+
+Il y a exactement 14 triangles rectangles formés à partir de deux points de coordonnées entières entre 0 et 2 inclus (0 ≤ x1, y1, x2, y2 ≤ 2).
+
+![ Cas ou n=2](https://projecteuler.net/project/images/p091_2.gif)
+
+Si on choisit 0 ≤ x1, y1, x2, y2 ≤ 50, combien de triangles rectangles peut-on former ?
+
+On affichera le résultat avec `print`.
+
+@[Triangles rectangles de coordonnées entières]({"stubs": ["Defis/Euler_91.py"], "command": "python3 Defis/Euler_91_Test.py"})
+
+---
