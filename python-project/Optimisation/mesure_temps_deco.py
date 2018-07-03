e56271ae62dd807d8a9fba279ec884c9a3676fc9
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-07-03 16:15:34 +0200

    Create mesure_temps_deco.py

---
+++ python-project/Optimisation/mesure_temps_deco.py
@@ -0,0 +1,18 @@
+from time import time 
+
+def chrono(fonction):
+    def lancer(*args, **kwargs):
+        t_i = time()
+        resultat = fonction(*args, **kwargs)
+        t_f = time()
+        print("temps d'execution (en s):", t_f-t_i)
+        return resultat
+    return lancer
+    
+@chrono
+def fonction_à_tester(n):
+    somme=0
+    for i in range(1,n):
+        if n%i==0:
+            somme+= i
+    return somme        
