c8dc0c38bab6cec8bb6edb075424b7bda2fa96b3
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-07-14 16:50:24 +0200

    Update Programme_calcul.py

--- python-project/Variables_et_fonctions/Programme_calcul.py
+++ python-project/Variables_et_fonctions/Programme_calcul.py
@@ -1,5 +1,9 @@
 def mon_programme(n):
-    #Ne pas toucher ce qui précède
-    #Les valeurs pour la variable en entrée seront automatiquement données
-    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    # Ne pas toucher ce qui précède
+    # Les valeurs pour la variable en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter
     pass
+    # Appuyer sur Run pour recevoir les instructions
+    
+    
+    
