ca8e05511660c56b0701fedce2473c3a23b9fd6d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-07-15 12:56:20 +0200

    Create Programme_calcul3_Correction.py

---
+++ python-project/Variables_et_fonctions/Programme_calcul3_Correction.py
@@ -0,0 +1,16 @@
+def mon_programme(n):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    pass  
+    a = 2 * n
+    print(a)
+    a += 5
+    print(a)
+    b = n**2
+    print(b)
+    a, b = b, a
+    print(a,b)
+    a, b = 3*a + 1, 2*a - b
+    print(a,b)
+    
