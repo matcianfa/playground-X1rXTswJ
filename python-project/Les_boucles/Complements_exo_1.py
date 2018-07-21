42544e45dc781d0fcb9f7f7b45cbf947ff158051
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-07-21 17:29:16 +0200

    Create Complements_exo_1.py

---
+++ python-project/Les_boucles/Complements_exo_1.py
@@ -0,0 +1,9 @@
+def mon_programme(liste):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour la variable liste seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
+    
+    
