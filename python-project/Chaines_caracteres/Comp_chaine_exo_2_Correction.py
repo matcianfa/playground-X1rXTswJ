7ed52e7d689cb03dec1b969f0cf457d79274f156
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-07-23 11:52:44 +0200

    Update Comp_chaine_exo_2_Correction.py

--- python-project/Chaines_caracteres/Comp_chaine_exo_2_Correction.py
+++ python-project/Chaines_caracteres/Comp_chaine_exo_2_Correction.py
@@ -1,11 +1,10 @@
 texte="azqcoiihcvobzerajzasoijixbougroizaziabxbncpvirizojasgpoofoabsvhcvxihosaojqxxbbxhjvvwiyagslxmxciabnxqlahxgavvwcbavvwcgsioidhkkgcgkjzbnvxgcahgcxaxgjhxwglja"
 
 
-solution=""
 for lettre in texte :
     if lettre == "z":
         nouvelle_lettre = "a"
     else :
         nouvelle_lettre = chr(ord(lettre)+1)
-    solution += nouvelle_lettre
+    print(nouvelle_lettre)
     
