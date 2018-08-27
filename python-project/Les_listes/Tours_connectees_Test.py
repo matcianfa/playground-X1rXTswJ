588f11e821a570622c4acabe1d5b1a6e9a755761
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-08-27 13:04:04 +0200

    Create Tours_connectees_Test.py

---
+++ python-project/Les_listes/Tours_connectees_Test.py
@@ -0,0 +1,69 @@
+module="Les_listes/Tours_connectees"
+
+import sys
+import io
+from ma_bao import *
+tester("from Tours_connectees import mon_programme",globals())
+
+#liste des couples input/output
+input_output=[\
+((2,[5]),5),\
+((3,[1,3]),3),\
+((4,[2,2,2]),8),\
+((5,[1,3,5,2]),30),\
+((7,[1,2,3,4,5,6]),720)\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print(x) pour afficher le resultat."
+
+#Afficher la correction
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+        pass
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    afficher_correction()
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
