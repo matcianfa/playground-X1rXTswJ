7b55b72273a5be74283efb4b001db54933dd5758
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-08-27 10:53:43 +0200

    Create Symetrie_centrale_Correction.py

---
+++ python-project/Variables_et_fonctions/Symetrie_centrale_Correction.py
@@ -0,0 +1,6 @@
+def mon_programme(x_p, y_p, x_q, y_q):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    print(2*x_q-x_p,2*y_q-y_p)
+    
