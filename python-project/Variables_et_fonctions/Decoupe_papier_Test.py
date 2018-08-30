862a21025009fd7abd61c9931f4bdd64bb65c06c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-08-30 09:28:00 +0200

    Create Decoupe_papier_Test.py

---
+++ python-project/Variables_et_fonctions/Decoupe_papier_Test.py
@@ -0,0 +1,70 @@
+module="Variables_et_fonctions/Decoupe_papier"
+
+import sys
+import io
+from ma_bao import *
+tester("from Decoupe_papier import mon_programme",globals())
+
+#liste des couples input/output
+input_output=[\
+((2,1),1),\
+((1,12),11),\
+((2,6),11),\
+((9,2),17),\
+((11,11),120),\
+((123456789,987654321),123456789*987654321 -1)\
+]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser print(x) pour afficher le resultat."
+
+#Afficher la correction
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+        pass
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    afficher_correction()
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp,outp in input_output:
+        sauvegarde_stdout=sys.stdout
+        sys.stdout=io.StringIO()
+        mon_programme(*inp)
+        count1 = sys.stdout.getvalue()[:-1]
+        sys.stdout=sauvegarde_stdout
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
