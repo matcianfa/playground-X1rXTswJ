be528751b1ec7a54111a24f1f457c9c03eda51fa
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-08-30 09:37:28 +0200

    Create Somme_telescopique.py

---
+++ python-project/Variables_et_fonctions/Somme_telescopique.py
@@ -0,0 +1,9 @@
+def mon_programme(n):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
+    
+    
