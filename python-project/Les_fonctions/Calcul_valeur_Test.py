51cc0c7a05bc3f62b73651104b02a542bca2894d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-10-26 10:40:24 +0200

    Create Calcul_valeur_Test.py

---
+++ python-project/Les_fonctions/Calcul_valeur_Test.py
@@ -0,0 +1,62 @@
+#Ne pas oublier de changer le module à importer
+module="Les_conditions/Calcul_valeur"
+
+import sys
+import io
+from ma_bao import *
+tester("from Calcul_valeur import f ",globals())
+
+def f_sol(x):
+  return 3*x**2+4*x-5
+  
+#liste des couples input/output
+input_output=[-4,3,0,1,5,1.5,18]
+
+
+#message d'aide si besoin
+help="N'oublie pas d'utiliser return pour afficher le resultat"
+
+#Afficher la correction
+def afficher_correction():
+    try:
+        with open(module+"_Correction.py", "r") as correction :
+            ligne="Voici un ou des exemples de corrections possibles"
+            send_msg("Exemple(s) de correction", ligne)
+            ligne="-------------------------------------------------"
+            send_msg("Exemple(s) de correction", ligne)
+            lignes=correction.read().split("\n")
+            for ligne in lignes:
+                send_msg("Exemple(s) de correction", ligne)
+    except:
+        pass
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    send_msg("Tests validés","Bravo !")
+    afficher_correction()
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      for inp in input_output:
+        outp=f_sol(inp)
+        count1=f(inp)
+        assert str(count1) == str(outp), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
+        send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
