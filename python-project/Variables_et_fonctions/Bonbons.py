a24303778850c248eda9bb7b1568279d22eadd85
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-11-22 17:24:49 +0100

    Create Bonbons.py

---
+++ python-project/Variables_et_fonctions/Bonbons.py
@@ -0,0 +1,4 @@
+def mon_programme(a,b):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter
