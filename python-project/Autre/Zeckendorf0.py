91c491ed2df1957f29876b9bb0d4d88bc6ebf28e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-09 18:57:02 +0100

    Create Zeckendorf0.py

---
+++ python-project/Autre/Zeckendorf0.py
@@ -0,0 +1,33 @@
+# Compléter la valeur de N ci-dessous pour afficher la décomposition de Zeckendorf de N
+
+N =   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Ne pas supprimer ce qui suit
+import Zeckendorf
