d88d2a7074558b41d3d72dfd7941fafeba15164e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-19 19:43:42 +0100

    Update Carreaux_mouvants.py

--- python-project/Variables_et_fonctions/Carreaux_mouvants.py
+++ python-project/Variables_et_fonctions/Carreaux_mouvants.py
@@ -1,4 +1,4 @@
-def mon_programme(L,V1,V2,q):
+def ma_fonction(L,V1,V2,q):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
