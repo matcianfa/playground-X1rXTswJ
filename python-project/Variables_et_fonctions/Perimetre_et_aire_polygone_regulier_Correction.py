d65ca1329a47fa19d2ff2700283cb0dbbb17e3a9
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-19 19:53:34 +0100

    Update Perimetre_et_aire_polygone_regulier_Correction.py

--- python-project/Variables_et_fonctions/Perimetre_et_aire_polygone_regulier_Correction.py
+++ python-project/Variables_et_fonctions/Perimetre_et_aire_polygone_regulier_Correction.py
@@ -1,5 +1,5 @@
-def mon_programme(n):
+def ma_fonction(n):
     coté=2*sin(pi/n)
     périmètre= n*coté
     aire=périmètre*cos(pi/n)/2
-    print(round(périmètre,2),round(aire,2))
+    return (round(périmètre,2),round(aire,2))
