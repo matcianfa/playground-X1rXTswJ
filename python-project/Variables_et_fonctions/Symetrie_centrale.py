8d02e5c9fa7f3a6066e9dd0ca545d71497d83790
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-19 20:07:32 +0100

    Update Symetrie_centrale.py

--- python-project/Variables_et_fonctions/Symetrie_centrale.py
+++ python-project/Variables_et_fonctions/Symetrie_centrale.py
@@ -1,4 +1,4 @@
-def mon_programme(x_p, y_p, x_q, y_q):
+def ma_fonction(x_p, y_p, x_q, y_q):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
