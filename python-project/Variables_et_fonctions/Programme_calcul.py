29c991333a159739f0287daafa9250138fe3e0db
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-19 20:04:57 +0100

    Update Programme_calcul.py

--- python-project/Variables_et_fonctions/Programme_calcul.py
+++ python-project/Variables_et_fonctions/Programme_calcul.py
@@ -1,4 +1,4 @@
-def ma_fonction(n):
+def mon_programme(n):
     # Ne pas toucher ce qui précède
     # Les valeurs pour la variable en entrée seront automatiquement données
     # Ecrire ci-dessous en n'oubliant pas d'indenter
