5ebc9b275eb0859054c41da7c1dafb646cf1f3a0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-19 19:51:14 +0100

    Update Passage_polaires_cartesiennes.py

--- python-project/Variables_et_fonctions/Passage_polaires_cartesiennes.py
+++ python-project/Variables_et_fonctions/Passage_polaires_cartesiennes.py
@@ -1,6 +1,10 @@
 from math import *
 
-def mon_programme(r,theta):
+def ma_fonction(r,theta):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
+    
+    
