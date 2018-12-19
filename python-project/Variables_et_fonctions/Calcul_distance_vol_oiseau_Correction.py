cad80b8148e706691869b42e28cae1e41f9e08e5
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-19 19:57:37 +0100

    Update Calcul_distance_vol_oiseau_Correction.py

--- python-project/Variables_et_fonctions/Calcul_distance_vol_oiseau_Correction.py
+++ python-project/Variables_et_fonctions/Calcul_distance_vol_oiseau_Correction.py
@@ -5,4 +5,4 @@ def ma_fonction(lat_a,long_a,lat_b,long_b):
     x=(long_b-long_a)*pi/180*cos((lat_a+lat_b)*pi/360)
     y=(lat_b-lat_a)*pi/180
     distance = rayon*sqrt(x**2+y**2)
-    return (round(distance,3))
+    return round(distance,3)
