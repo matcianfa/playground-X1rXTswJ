b435b9e133f608939389432a4af4ec04fa2d326c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 16:16:27 +0100

    Update nombre_pair.py

--- python-project/Les_conditions/nombre_pair.py
+++ python-project/Les_conditions/nombre_pair.py
@@ -1,5 +1,7 @@
-def mon_programme(n):
+def ma_fonction(n):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+
     
