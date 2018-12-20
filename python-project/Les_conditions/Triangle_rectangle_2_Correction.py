36e61847462f3442e0974435324c81f282001142
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 16:13:11 +0100

    Update Triangle_rectangle_2_Correction.py

--- python-project/Les_conditions/Triangle_rectangle_2_Correction.py
+++ python-project/Les_conditions/Triangle_rectangle_2_Correction.py
@@ -1,5 +1,5 @@
-def mon_programme(x_a,y_a,x_b,y_b,x_c,y_c):
+def ma_fonction(x_a,y_a,x_b,y_b,x_c,y_c):
     if (x_b-x_a)*(x_c-x_a)+(y_b-y_a)*(y_c-y_a)==0 or (x_a-x_b)*(x_c-x_b)+(y_a-y_b)*(y_c-y_b)==0 or (x_b-x_c)*(x_a-x_c)+(y_b-y_c)*(y_a-y_c)==0:
-        print('RECTANGLE')
+        return 'RECTANGLE'
     else :
-        print('PAS RECTANGLE')
+        return 'PAS RECTANGLE'
