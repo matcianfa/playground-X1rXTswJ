4d24cf11e1d5ff7e24cd1689b4ebff67f8523244
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 16:33:20 +0100

    Update Nature_d_un_triangle_Test.py

--- python-project/Les_conditions/Nature_d_un_triangle_Test.py
+++ python-project/Les_conditions/Nature_d_un_triangle_Test.py
@@ -32,7 +32,7 @@ def test():
         for valeur in valeurs_a_tester:
             rep=f(*valeur)
             sol=f_sol(*valeur)
-            assert str(rep) == str(sol), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(",".join([str(val) for val in valeur]),str(rep),str(sol))
+            assert str(rep) == str(sol) or (str(rep)=="ISOCELE" and str(rep)=="TRIANGLE D'OR"), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(",".join([str(val) for val in valeur]),str(rep),str(sol))
             send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(",".join([str(val) for val in valeur]),str(rep)))
         success(chemin+module)
     except AssertionError as e:
