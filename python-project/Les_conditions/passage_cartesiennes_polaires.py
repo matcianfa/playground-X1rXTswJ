2089c3e03cf4d30699ef0dcc843fbafb115f329c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 16:18:09 +0100

    Update passage_cartesiennes_polaires.py

--- python-project/Les_conditions/passage_cartesiennes_polaires.py
+++ python-project/Les_conditions/passage_cartesiennes_polaires.py
@@ -1,6 +1,9 @@
 from math import *
 
-def mon_programme(x,y):
+def ma_fonction(x,y):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
