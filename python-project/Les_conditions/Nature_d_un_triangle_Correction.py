51b8a71ec02bab868eccff63257eb857b0598389
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 15:59:16 +0100

    Update Nature_d_un_triangle_Correction.py

--- python-project/Les_conditions/Nature_d_un_triangle_Correction.py
+++ python-project/Les_conditions/Nature_d_un_triangle_Correction.py
@@ -1,14 +1,16 @@
-def mon_programme(a,b,c):
+from math import *
+
+def ma_fonction(a,b,c):
     if a==b==c:
-        print('EQUILATERAL')
+        return 'EQUILATERAL'
     elif a==b or a==c or b==c:
         if round(a**2,5)==round(b**2+c**2,5) or round(b**2,5)==round(a**2+c**2,5) or round(c**2,5)==round(a**2+b**2,5) :
-            print('RECTANGLE ISOCELE')
+            return 'RECTANGLE ISOCELE'
         elif a/b==(1+sqrt(5))/2 or b/a==(1+sqrt(5))/2 or a/c==(1+sqrt(5))/2 or c/a==(1+sqrt(5))/2:
-            print('TRIANGLE D'OR')
+            return 'TRIANGLE D\'OR'
         else:
-            print('ISOCELE')
+            return 'ISOCELE'
     elif round(a**2,5)==round(b**2+c**2,5) or round(b**2,5)==round(a**2+c**2,5) or round(c**2,5)==round(a**2+b**2,5) :
-        print('RECTANGLE')
+        return 'RECTANGLE'
     else:
-        print('QUELCONQUE')
+        return 'QUELCONQUE'
