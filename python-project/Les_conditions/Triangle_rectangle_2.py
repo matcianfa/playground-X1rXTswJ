fa4387543307ff2b32dd3b015043ee931889897f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 16:12:39 +0100

    Update Triangle_rectangle_2.py

--- python-project/Les_conditions/Triangle_rectangle_2.py
+++ python-project/Les_conditions/Triangle_rectangle_2.py
@@ -1,4 +1,7 @@
-def mon_programme(x_a,y_a,x_b,y_b,x_c,y_c):
+def ma_fonction(x_a,y_a,x_b,y_b,x_c,y_c):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
+    
