fab87e0778a6902518002369818adffdddb5ed4e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-20 15:50:14 +0100

    Update Conversion_Celsius_Fahrenheit.py

--- python-project/Les_conditions/Conversion_Celsius_Fahrenheit.py
+++ python-project/Les_conditions/Conversion_Celsius_Fahrenheit.py
@@ -1,4 +1,7 @@
-def mon_programme(t,n):
+def ma_fonction(t,n):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
+    
