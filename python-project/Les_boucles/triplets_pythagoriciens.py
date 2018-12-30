32b3f9fb7a190ae8afec80144a4ba686d2175ec2
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:52:19 +0100

    Update triplets_pythagoriciens.py

--- python-project/Les_boucles/triplets_pythagoriciens.py
+++ python-project/Les_boucles/triplets_pythagoriciens.py
@@ -1,4 +1,7 @@
-def mon_programme(c):
+def ma_fonction(c):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
+    
