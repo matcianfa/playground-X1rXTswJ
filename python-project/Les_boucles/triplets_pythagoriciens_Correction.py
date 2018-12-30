ae6fd460e512da4c256e40bb5e3ef0e2d6d733da
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:52:43 +0100

    Update triplets_pythagoriciens_Correction.py

--- python-project/Les_boucles/triplets_pythagoriciens_Correction.py
+++ python-project/Les_boucles/triplets_pythagoriciens_Correction.py
@@ -1,7 +1,7 @@
-def mon_programme(c):
+def ma_fonction(c):
     compteur=0
     for a in range(c+1):
         for b in range(c+1):
             if a**2+b**2==c**2:
                 compteur+=1
-    print(compteur)
+    return compteur
