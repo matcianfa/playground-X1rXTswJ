6f1824844156309cbd4f471c43840be1f2117c09
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:27:19 +0100

    Update Factorielle_Correction.py

--- python-project/Les_boucles/Factorielle_Correction.py
+++ python-project/Les_boucles/Factorielle_Correction.py
@@ -1,7 +1,7 @@
-def mon_programme(n):
+def ma_fonction(n):
     if n==0 : resultat=1
     else :
         resultat=1
         for i in range(1,n+1):
             resultat*=i
-    print(resultat)
+    return resultat
