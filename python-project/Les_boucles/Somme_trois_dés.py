3c22a46a972501026d31e93518686c4e740d88e8
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:47:37 +0100

    Update Somme_trois_dés.py

--- python-project/Les_boucles/Somme_trois_dés.py
+++ python-project/Les_boucles/Somme_trois_dés.py
@@ -1,4 +1,7 @@
-def mon_programme(k):
+def ma_fonction(k):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
+    
