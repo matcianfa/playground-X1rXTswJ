32fb5bfe570add0272166db093da513c682a03dc
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:28:34 +0100

    Update Gardner.py

--- python-project/Les_boucles/Gardner.py
+++ python-project/Les_boucles/Gardner.py
@@ -1,4 +1,7 @@
-def mon_programme(e):
+def ma_fonction(e):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+
+    
+    
