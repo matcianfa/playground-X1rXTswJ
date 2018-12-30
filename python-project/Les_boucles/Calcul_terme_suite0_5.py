0d33d761df47bc33c4a0b6dac31620881c606a5d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:10:07 +0100

    Update Calcul_terme_suite0_5.py

--- python-project/Les_boucles/Calcul_terme_suite0_5.py
+++ python-project/Les_boucles/Calcul_terme_suite0_5.py
@@ -1,5 +1,5 @@
 
-def mon_programme(n,u_0):
+def ma_fonction(n,u0):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
