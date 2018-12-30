b7b62e29f57bcfb07f623924caf7ae38ed01a98e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:22:07 +0100

    Update Complements_exo_1.py

--- python-project/Les_boucles/Complements_exo_1.py
+++ python-project/Les_boucles/Complements_exo_1.py
@@ -1,4 +1,4 @@
-def mon_programme(liste):
+def ma_fonction(liste):
     #Ne pas toucher ce qui précède
     #Les valeurs pour la variable liste seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
