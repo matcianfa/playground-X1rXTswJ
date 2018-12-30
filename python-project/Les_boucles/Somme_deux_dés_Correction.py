c961deb14033eeecb7dba720641dcf63c718d7f4
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:45:56 +0100

    Update Somme_deux_dés_Correction.py

--- python-project/Les_boucles/Somme_deux_dés_Correction.py
+++ python-project/Les_boucles/Somme_deux_dés_Correction.py
@@ -1,7 +1,7 @@
-def mon_programme(k):
+def ma_fonction(k):
     compteur=0
     for i in range(1,7):
         for j in range(1,7):
             if i+j==k:
                 compteur+=1
-    print(compteur)
+    return compteur
