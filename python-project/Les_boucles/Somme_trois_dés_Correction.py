cb2799b415789d877e287119b69fe68703a3fa53
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-30 15:48:00 +0100

    Update Somme_trois_dés_Correction.py

--- python-project/Les_boucles/Somme_trois_dés_Correction.py
+++ python-project/Les_boucles/Somme_trois_dés_Correction.py
@@ -1,8 +1,8 @@
-def mon_programme(k):
+def ma_fonction(k):
     compteur=0
     for i in range(1,7):
         for j in range(1,7):
             for l in range(1,7):
                 if i+j+l==k:
                     compteur+=1
-    print(compteur)
+    return compteur
