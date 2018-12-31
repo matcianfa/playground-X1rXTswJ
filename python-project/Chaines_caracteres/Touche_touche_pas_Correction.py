968e04500d040b0c2f34e85d97b515660c46118e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-31 15:41:04 +0100

    Update Touche_touche_pas_Correction.py

--- python-project/Chaines_caracteres/Touche_touche_pas_Correction.py
+++ python-project/Chaines_caracteres/Touche_touche_pas_Correction.py
@@ -1,9 +1,10 @@
-def mon_programme(mot):
+def ma_fonction(mot):
+    #On fait disparaitre tous les labiales muettes
     mot=mot.replace('COMPT','')
     mot=mot.replace('MPH','')
     mot=mot.replace('PH','')
     mot=mot[:-3]+mot[-3:].replace('MP','')
     if 'P' in mot[:-1] or 'M' in mot or 'B' in mot :
-        print('TOUCHE')
+        return 'TOUCHE'
     else:
-        print('TOUCHE PAS')
+        return 'TOUCHE PAS'
