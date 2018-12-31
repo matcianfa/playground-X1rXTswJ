0c41a7b249553bf8e6fa955224d9be897f79a116
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-31 15:38:32 +0100

    Update Touche_touche_pas.py

--- python-project/Chaines_caracteres/Touche_touche_pas.py
+++ python-project/Chaines_caracteres/Touche_touche_pas.py
@@ -1,4 +1,7 @@
-def mon_programme(mot):
+def ma_fonction(mot):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
