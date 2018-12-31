8c40eb2afb88e8f15927ecebb9d392b6349ce1e2
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-31 14:50:56 +0100

    Update Anacycliques.py

--- python-project/Chaines_caracteres/Anacycliques.py
+++ python-project/Chaines_caracteres/Anacycliques.py
@@ -1,4 +1,7 @@
-def mon_programme(mot_1,mot_2):
+def ma_fonction(mot1,mot2):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
