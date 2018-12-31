ed54094820d88bb8b2e50fa4bd445ff6b33bc876
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-31 14:55:34 +0100

    Update Carte_bancaire.py

--- python-project/Chaines_caracteres/Carte_bancaire.py
+++ python-project/Chaines_caracteres/Carte_bancaire.py
@@ -1,4 +1,6 @@
-def mon_programme(numero):
+def ma_fonction(numero):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
