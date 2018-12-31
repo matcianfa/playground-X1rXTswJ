f0737841e97ffee91c7ab27092129e50bc5a8043
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-31 15:01:05 +0100

    Update Codage_vigenere.py

--- python-project/Chaines_caracteres/Codage_vigenere.py
+++ python-project/Chaines_caracteres/Codage_vigenere.py
@@ -1,4 +1,7 @@
-def mon_programme(mot_clé,mot):
+def ma_fonction(mot_clé,mot):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
