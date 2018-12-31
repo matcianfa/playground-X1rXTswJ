f2761adcef6626d87ad595f5164067397ac8cb1f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2018-12-31 14:51:30 +0100

    Update Anacycliques_Correction.py

--- python-project/Chaines_caracteres/Anacycliques_Correction.py
+++ python-project/Chaines_caracteres/Anacycliques_Correction.py
@@ -1,9 +1,9 @@
-def mon_programme(mot_1,mot_2):
+def ma_fonction(mot_1,mot_2):
     mot_1_inversé=''
     #Je renverse mot_1
     for lettre in mot_1:
         mot_1_inversé=lettre+mot_1_inversé
     if mot_1_inversé==mot_2:
-        print('ANACYCLIQUE')
+        return 'ANACYCLIQUE'
     else:
-        print('PAS ANACYCLIQUE')
+        return 'PAS ANACYCLIQUE'
