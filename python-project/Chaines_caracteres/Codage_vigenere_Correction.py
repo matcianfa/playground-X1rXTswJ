11a4acbd8001cc0454972c6e6db21125f77be6e7
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-02 10:47:23 +0100

    Update Codage_vigenere_Correction.py

--- python-project/Chaines_caracteres/Codage_vigenere_Correction.py
+++ python-project/Chaines_caracteres/Codage_vigenere_Correction.py
@@ -1,5 +1,5 @@
 def ma_fonction(mot_clé,mot):
-    mot_codé=""
+    mot_codé=''
     for k in range(len(mot)):
         mot_codé+=chr((ord(mot[k])-65*2+ord(mot_clé[k%len(mot_clé)]))%26+65)
     return mot_codé
