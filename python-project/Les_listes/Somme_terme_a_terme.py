5ea60cdd98ca0ddb5a3a8271213cdeaf22657d30
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-03 15:35:53 +0100

    Update Somme_terme_a_terme.py

--- python-project/Les_listes/Somme_terme_a_terme.py
+++ python-project/Les_listes/Somme_terme_a_terme.py
@@ -1,5 +1,6 @@
-def mon_programme(liste_1, liste_2):
+def ma_fonction(liste_1, liste_2):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
 
