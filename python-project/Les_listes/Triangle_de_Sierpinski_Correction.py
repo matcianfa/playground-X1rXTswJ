006835bfa8d03c131e893253f705d2a1bdf53e46
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-03 12:41:33 +0100

    Update Triangle_de_Sierpinski_Correction.py

--- python-project/Les_listes/Triangle_de_Sierpinski_Correction.py
+++ python-project/Les_listes/Triangle_de_Sierpinski_Correction.py
@@ -1,12 +1,1 @@
-#Vous pouvez modifier la dimension. Le mieux est de prendre une puissance de 2
-dimension=2**4
 
-def ma_fonction(n):
-    ligne=[1]
-    for i in range(n):
-        ligne_temporaire=[1]
-        for j in range(1,len(ligne)):
-            ligne_temporaire.append(ligne[j-1]+ligne[j])
-        ligne_temporaire.append(1)
-        ligne=list(ligne_temporaire)
-    return ligne
