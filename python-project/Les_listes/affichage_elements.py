b0a965a52bcfe2a9be55ee72c3df840fe796b31c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-03 15:53:14 +0100

    Update affichage_elements.py

--- python-project/Les_listes/affichage_elements.py
+++ python-project/Les_listes/affichage_elements.py
@@ -1,7 +1,8 @@
-def mon_programme(liste):
+def ma_fonction(liste):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
 
     
     
