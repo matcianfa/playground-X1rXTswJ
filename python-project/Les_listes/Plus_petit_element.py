013cc9e2f3ad496904b80974facd12c5e77f0ad3
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-03 15:33:25 +0100

    Update Plus_petit_element.py

--- python-project/Les_listes/Plus_petit_element.py
+++ python-project/Les_listes/Plus_petit_element.py
@@ -1,5 +1,6 @@
-def mon_programme(liste):
+def ma_fonction(liste):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
 
