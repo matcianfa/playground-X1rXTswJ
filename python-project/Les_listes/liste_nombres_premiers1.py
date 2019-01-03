6ac6f9dbd18fc0daccca152d756091b3eb0eedaa
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-03 15:56:39 +0100

    Update liste_nombres_premiers1.py

--- python-project/Les_listes/liste_nombres_premiers1.py
+++ python-project/Les_listes/liste_nombres_premiers1.py
@@ -1,7 +1,8 @@
 from Est_premier import est_premier
 
-def mon_programme(a,b):
+def ma_fonction(a,b):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
+    
 
