30cee96ba1b47a8eb784a2d309a5d07981916b1c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-01-03 15:37:07 +0100

    Update Tours_connectees.py

--- python-project/Les_listes/Tours_connectees.py
+++ python-project/Les_listes/Tours_connectees.py
@@ -1,4 +1,4 @@
-def mon_programme(n,liste):
+def ma_fonction(n,liste):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter
