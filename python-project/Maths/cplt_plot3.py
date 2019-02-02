a1a2927c9afc97599131c752eceb336a69b8656c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-02-02 12:40:48 +0100

    Create cplt_plot3.py

---
+++ python-project/Maths/cplt_plot3.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-2, 2, 100)
+y = 2*x**2+3*x-4
+
+plt.plot(x,y,label="Courbe n°1")
+
+plt.xlim(-3, 3)
+plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi], [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+plt.yticks(np.linspace(-6, 12, 13))
+plt.xlabel("axe des abscisses")
+plt.ylabel("axe des ordonnées")
+plt.grid()
+plt.legend(loc='upper center')
+plt.show()
