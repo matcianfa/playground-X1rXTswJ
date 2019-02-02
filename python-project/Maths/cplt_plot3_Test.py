0f548df75d6e1a7d203d1160058bb60f23ae7c3f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-02-02 12:41:21 +0100

    Create cplt_plot3_Test.py

---
+++ python-project/Maths/cplt_plot3_Test.py
@@ -0,0 +1,39 @@
+#Ne pas oublier de changer le module à importer
+import matplotlib
+matplotlib.use('agg')
+import sys
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+#message d'aide si besoin
+help=""
+
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    print("TECHIO> open -s /project/target/ index.html")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      fig=plt.figure()
+      import cplt_plot3
+      fig.savefig('output.png', dpi=fig.dpi)
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
