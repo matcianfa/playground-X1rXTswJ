608505e25d6d2541b905bd92b9b72b8ca987f67c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-02-02 14:25:07 +0100

    Update cplt_plot4.py

--- python-project/Maths/cplt_plot4.py
+++ python-project/Maths/cplt_plot4.py
@@ -7,6 +7,7 @@ y = 2*x**2+3*x-4
 plt.plot(x,y)
 plt.ylim(-8,11)
 
+plt.text(1,4.5,"Du texte",rotation=55)
 plt.annotate(r"Le minimum atteint en $-\frac{b}{2a}$", xy=(-3/4, -41/8),xytext=(0, -5.5), arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=-.4"))
 plt.annotate("Annotation 1",xy=(0,0),xytext=(-1,3))
 
