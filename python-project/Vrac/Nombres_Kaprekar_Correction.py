bb7e83bf89f3bfd1c1c91dea25c2699895cce020
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-03-05 22:19:04 +0100

    Update Nombres_Kaprekar_Correction.py

--- python-project/Vrac/Nombres_Kaprekar_Correction.py
+++ python-project/Vrac/Nombres_Kaprekar_Correction.py
@@ -2,7 +2,7 @@ def ma_fonction(n):
     carré=str(n**2)
     for i in range(1,len(carré)):
         if int(carré[:i])+int(carré[i:])==n:
-            return "KAPREKAR"
+            return 'KAPREKAR'
             break
     else:
-        return "PAS KAPREKAR"
+        return 'PAS KAPREKAR'
