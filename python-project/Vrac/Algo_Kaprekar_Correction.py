fcbd4eb1a3b47dbebd6a191cc572415867e7f936
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-03-05 22:21:13 +0100

    Update Algo_Kaprekar_Correction.py

--- python-project/Vrac/Algo_Kaprekar_Correction.py
+++ python-project/Vrac/Algo_Kaprekar_Correction.py
@@ -4,10 +4,10 @@ def ma_fonction(n):
     liste_résultats=[]
     while n not in liste_résultats:
         liste_résultats.append(n)
-        a=int("".join(sorted([chiffre for chiffre in n],reverse=True)))
-        b=int("".join(sorted([chiffre for chiffre in n])))
+        a=int(''.join(sorted([chiffre for chiffre in n],reverse=True)))
+        b=int(''.join(sorted([chiffre for chiffre in n])))
         n=str(a-b)
         #On rajoute les 0 pour garder la même longueur que le chiffre du départ
-        n="0"*(longueur-len(n))+n
+        n='0'*(longueur-len(n))+n
     indice=liste_résultats.index(n)#On récupère à partir de quel indice ca boucle
     return [int(nombre) for nombre in liste_résultats[indice:]] #Attention on demande une liste d'entier et non de chaine de caractères
