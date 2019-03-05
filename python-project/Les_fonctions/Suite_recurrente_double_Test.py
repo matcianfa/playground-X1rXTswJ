27059a3b4f08af334b1e611d381d69b77ee77603
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-03-05 19:38:29 +0100

    Update Suite_recurrente_double_Test.py

--- python-project/Les_fonctions/Suite_recurrente_double_Test.py
+++ python-project/Les_fonctions/Suite_recurrente_double_Test.py
@@ -1,5 +1,5 @@
 # A modifier si besoin
-nom_fonction="ma_fonction"
+nom_fonction="u"
 
 #liste des valeurs à tester
 # Attention de bien mettre dans un tuplet ou une liste les valeurs à tester même si la fonction n'a qu'un argument.
