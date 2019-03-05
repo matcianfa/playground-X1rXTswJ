0b9471b8441adac84aec2aa376317b7c497f9f5a
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-03-05 20:08:02 +0100

    Update tri_stupide_Test.py

--- python-project/Vrac/tri_stupide_Test.py
+++ python-project/Vrac/tri_stupide_Test.py
@@ -1,49 +1,45 @@
-#Ne pas oublier de changer le module à importer
-from tri_stupide import mon_programme
-import sys
-import io
+# A modifier si besoin
+nom_fonction="ma_fonction"
 
-
-#liste des couples input/output
-input_output=[\
-([1,5,6],[1,5,6]),\
-([1,5,1,5,6],[1,1,5,5,6]),\
-([3,2,5,7,10,1],[1,2,3,5,7,10]),\
-([8,7,6,5,4,3,2,1],[1,2,3,4,5,6,7,8]),\
-([1],[1])\
-]
-
+#liste des valeurs à tester
+# Attention de bien mettre dans un tuplet ou une liste les valeurs à tester même si la fonction n'a qu'un argument.
+valeurs_a_tester=[[1,5,6],[1,5,1,5,6],[3,2,5,7,10,1],[8,7,6,5,4,3,2,1],[1]]
 
 #message d'aide si besoin
-help="N'oublie pas d'utiliser return pour afficher le resultat. Attention aux pièges : liste vide et avec un seul élément !"
+help="N'oublie pas d'utiliser return pour renvoyer le resultat."
 
+#------------------------------------
+# Les imports
+import sys
+# Ma boite à outils
+from ma_bao import * 
+# Donne les noms du dossier et du module (automatiquement avec __file__
+chemin,module=donner_chemin_nom(__file__)
+# On teste s'il n'y a pas d'erreurs de synthaxe etc. et on les montre si besoin
+tester("from {} import *".format(module),globals()) 
+# On renomme ma fonction f
+f=eval(nom_fonction)
+# Si le mot de passe est bon on affiche la correction
+try :  
+    cheat(chemin+module,mdp) 
+except: pass
+# On récupère la fonction solution
+exec("from {}_Correction import {} as f_sol".format(module,nom_fonction))
 
-
-def send_msg(channel, msg):
-    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
-
-
-def success():
-    send_msg("Tests validés","Bravo !")
-    print("TECHIO> success true")
-
-
-def fail():
-    print("TECHIO> success false")
-    
-
+#--------------------------------------
 def test():
     try:
-      for inp,outp in input_output:
-        count1 = mon_programme(list(inp))
-        assert count1 == outp, "En testant la liste {} le résultat obtenu est {} au lieu de {}".format(str(inp),str(count1),str(outp))
-        send_msg("Tests validés","En testant la liste {} le résultat obtenu est bien {}".format(str(inp),str(count1)))
-      success()
+        for valeur in valeurs_a_tester:
+            rep=f(*valeur)
+            sol=f_sol(*valeur)
+            assert str(rep) == str(sol), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(",".join([str(val) for val in valeur]),str(rep),str(sol))
+            send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(",".join([str(val) for val in valeur]),str(rep)))
+        success(chemin+module)
     except AssertionError as e:
-      fail()
-      send_msg("Oops! ", e)
-      if help:
-        send_msg("Aide 💡", help)
+        fail()
+        send_msg("Oops! ", e)
+        if help:
+            send_msg("Aide 💡", help)
 
-
+#--------------------------------------
 if __name__ == "__main__": test()
