e1296210df5a045870445ed9749ce9b4512b624f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-05 17:23:17 +0200

    Update ma_bao.py

--- python-project/Premiere/Second_degre/ma_bao.py
+++ python-project/Premiere/Second_degre/ma_bao.py
@@ -6,6 +6,7 @@ code="e44f8cf63970db5c2df0a18153bcdf49"
 
 # Permet de récupérer le chemin d'accès et le nom du module. A utiliser avec __file__
 def donner_chemin_nom(file) :
+    send_msg(test,file)
     m = re.search(r"(?P<dossier>\w+/)(?P<module>\w+)_Test.py", file)
     if m is not None:
         return m.group('dossier'),m.group('module')
