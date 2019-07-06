48c59e958d8028387604e370519aa556f43a3746
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-06 12:00:54 +0200

    Update Racines_poly_premier_et_second_degre.py

--- python-project/Premiere/Second_degre/Racines_poly_premier_et_second_degre.py
+++ python-project/Premiere/Second_degre/Racines_poly_premier_et_second_degre.py
@@ -2,3 +2,8 @@ def ma_fonction(liste):
     #Ne pas toucher ce qui précède
     #Les valeurs pour les variables en entrée seront automatiquement données
     #Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+
+    
+    
+    
+    
