712d4f8a8b097caf57068d7299a0ee4ad512acff
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-10 17:03:53 +0200

    Create espacer.py

---
+++ python-project/TP/espacer.py
@@ -0,0 +1,6 @@
+def espacer(mot):
+    #Ne pas toucher ce qui précède
+    #Les valeurs pour les variables en entrée seront automatiquement données
+    #Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+
