5ebe4ce2cf3a979564d78a2284711865177984e4
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-10 18:45:50 +0200

    Update pendu_Test.py

--- python-project/TP/pendu_Test.py
+++ python-project/TP/pendu_Test.py
@@ -1,5 +1,6 @@
 from pendu import * # On  importe les fonctions créées
 import random # Pour choisir aléatoirement
+from ma_bao import * # Simplement pour les fonctions fail() et success() qui modifie la couleur de la barre à le fin
 
 # --------------------- Constantes
 #On charge le dictionnaire ne contenant que les mots entre 7 et 10 lettres
@@ -10,12 +11,17 @@ nb_erreurs_max=7 # Maximum d'erreur possible avant d'etre pendu
 mot_choisi= random.choice(dico) # On choisit un mot au hasard dans notre dictionnaire
 mot_partiel="_"*len(mot_choisi) # On crée notre mot caché en remplaçant toutes les lettres par des _
 
+#--------------------- Les graphiques (3D bien sûr)
+graphiques=["  --------------\n    |        \n    |        \n    |       \n    |       \n    |      \n    |        \n    |        \n    |       \n   /|\     \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  --------------\n    |        |\n    |        |\n    |       \n    |       \n    |      \n    |        \n    |        \n    |       \n   /|\     \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  --------------\n    |        |\n    |        |\n    |       / \ \n    |       \_/\n    |      \n    |        \n    |        \n    |       \n   /|\     \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  --------------\n    |        |\n    |        |\n    |       / \ \n    |       \_/\n    |        |\n    |        |\n    |        |\n    |       \n   /|\    \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  --------------\n    |        |\n    |        |\n    |       / \ \n    |       \_/\n    |      __|\n    |        |\n    |        |\n    |       \n   /|\     \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  ------\--------\n    |        |\n    |        |\n    |       / \ \n    |       \_/\n    |      __|__\n    |        |\n    |        |\n    |       \n   /|\     \n  / | \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  --------------\n    |        |\n    |        |\n    |       / \ \n    |       \_/\n    |      __|__\n    |        |\n    |        |\n    |       / \n   /|\     /   \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n","  --------------\n    |        |\n    |        |\n    |       / \ \n    |       \o/\n    |      __|__\n    |        |\n    |        |\n    |       / \ \n   /|\     /   \ \n  / | \ \n /  |  \ \n~~~~~~~~~~~~~~~~~~~~~\n"]
+
+
 
 if __name__ == "__main__": 
     nb_erreurs = 0 # Pour compter le nombre d'erreur
+    print(graphiques[nb_erreurs])
     while "_" in mot_partiel and nb_erreurs < nb_erreurs_max :
         print("\nMot à trouver :",espacer(mot_partiel))
-        lettre=input("Donner une lettre").upper()
+        lettre=input("Donner une lettre : ").upper()
 
         # Si la lettre a déjà été trouvée, on retente
         if lettre in mot_partiel :
@@ -31,9 +37,12 @@ if __name__ == "__main__":
             nb_erreurs+=1
             print(lettre,"n'est pas dans le mot")
             print("Vous avez faire {} erreurs sur {}".format(nb_erreurs,nb_erreurs_max))
+        print(graphiques[nb_erreurs])
 
     if "_" in mot_partiel :
         print("Perdu!")
         print("Le mot était ", mot_choisi)
+        print("TECHIO> success false") # Fonction interne à ce site : pour afficher la barre en rouge
     else :
         print("Bravo !")
+        print("TECHIO> success true") # Fonction interne à ce site : pour afficher la barre en vert
