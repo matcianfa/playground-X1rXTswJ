0e0889d6ac287f6129d547666682cbac3404f0ea
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-14 17:38:19 +0200

    Update donner_solution_Test.py

--- python-project/TP/donner_solution_Test.py
+++ python-project/TP/donner_solution_Test.py
@@ -11,7 +11,7 @@ with open("TP/dictionnaire7-10.txt",mode="r") as dic:
 # Attention de bien mettre dans un tuplet ou une liste les valeurs à tester même si la fonction n'a qu'un argument.
 valeurs_a_tester=[["A Z R T Y U P Q S D".split(),"_______",dico],[["U","A","I","X","Z","C","Y","V","K"],"_AI___",dico],["L A Z E R T Y U I O P Q S".split(),"LAITUES",dico],["A Z E R T Y".split(),"AAA____",dico]]
 
-def verif(mot,lettres,mot_partiel,dictionnaire):
+def verif(mot2,lettres,mot_partiel,dictionnaire):
     """
     Vérifie si le mot proposé vérifie les critères
     """
@@ -25,7 +25,7 @@ def verif(mot,lettres,mot_partiel,dictionnaire):
         else :
             reponses.append(mot)
     if reponses:
-        if mot in reponses : return True
+        if mot2 in reponses : return True
         else : return False
     else : return True
 
