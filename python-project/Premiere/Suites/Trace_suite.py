70c485bbb1007f161aabec8bcd26a22d7d776181
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-15 18:27:23 +0200

    Create Trace_suite.py

---
+++ python-project/Premiere/Suites/Trace_suite.py
@@ -0,0 +1,15 @@
+# Compléter le script ci dessous en calculant les termes de la suite (Un)
+# puis en créant correctement les listes X et Y
+
+
+
+
+
+
+X= ...
+Y= ...
+
+import matplotlib.pyplot as plt
+plt.scatter(X,Y)
+plt.show()
+
