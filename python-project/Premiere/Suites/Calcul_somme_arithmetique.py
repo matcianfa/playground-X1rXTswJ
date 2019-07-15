b7dae174cf23521a09b8de84c7e09aad80455e1c
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-15 09:36:22 +0200

    Update Calcul_somme_arithmetique.py

--- python-project/Premiere/Suites/Calcul_somme_arithmetique.py
+++ python-project/Premiere/Suites/Calcul_somme_arithmetique.py
@@ -1,4 +1,4 @@
-def ma_fonction(p,q):
+def ma_fonction(u0,r,p,q):
     # Ne pas toucher ce qui précède
     # Les valeurs pour les variables en entrée seront automatiquement données
     # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
