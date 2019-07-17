69c4d25d574d91ae6eb54fc2b0e056cd82df8892
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-17 18:02:09 +0200

    Create VanEck_graphique.py

---
+++ python-project/TP/VanEck_graphique.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# Indice jusqu'auquel on calcule les termes de la suite
+N = 100000
+
+# Copier coller ci dessous votre fonction VanEck(n)
+
+
+
+
+
+
+
+
+# A compléter :
+X = ...
+Y = ...
+
+# A modifier si besoin :
+plt.plot(X,Y)
+plt.show()
