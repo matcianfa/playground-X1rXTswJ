5fa15a073f2b6e0a18f128509424b8795ca5cb68
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-17 14:33:12 +0200

    Create VanEck.py

---
+++ python-project/TP/VanEck.py
@@ -0,0 +1,7 @@
+def VanEck(n):
+    # Ne pas toucher ce qui précède
+    # Les valeurs pour les variables en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+
+
