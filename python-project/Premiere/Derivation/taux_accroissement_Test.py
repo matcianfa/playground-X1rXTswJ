63d9f695b2ec3593bb0aad7d9329832333a7a092
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-19 12:18:03 +0200

    Update taux_accroissement_Test.py

--- python-project/Premiere/Derivation/taux_accroissement_Test.py
+++ python-project/Premiere/Derivation/taux_accroissement_Test.py
@@ -3,7 +3,7 @@ nom_fonction="ma_fonction"
 
 #liste des valeurs à tester
 # Attention de bien mettre dans un tuplet ou une liste les valeurs à tester même si la fonction n'a qu'un argument.
-valeurs_a_tester=[["x**2",1],["x**3",2],["x**0.5",1],["1/x",10],["3*x+4",5]]
+valeurs_a_tester=[["x**2",1,0.1],["x**3",2,0.01],["x**0.5",1,0.05],["1/x",10,0.01],["3*x+4",5,-0.01]]
 
 
 #message d'aide si besoin
@@ -33,10 +33,10 @@ exec("from {}_Correction import {} as f_sol".format(module,nom_fonction))
 def test():
     try:
         for valeur in valeurs_a_tester:
-            a,b=valeur
+            a,b,c=valeur
             a= eval("lambda x : "+a)
-            rep=f(a,b)
-            sol=f_sol(a,b)
+            rep=f(a,b,c)
+            sol=f_sol(a,b,c)
             assert str(rep) == str(sol), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(",".join([str(val) for val in valeur]),str(rep),str(sol))
             send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(",".join([str(val) for val in valeur]),str(rep)))
         success(chemin+module)
