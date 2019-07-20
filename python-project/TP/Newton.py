dc8d04d045738459bb8db8b54a475527e8dccf0d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-20 09:08:37 +0200

    Update Newton.py

--- python-project/TP/Newton.py
+++ python-project/TP/Newton.py
@@ -1,5 +1,11 @@
+# Copier ci dessous votre fonction deriver(f,a,h)
+
+
+
+
+
+
 def Newton(f,x0,n):
-    # Ne pas toucher ce qui précède
     # Les valeurs pour les variables en entrée seront automatiquement données
     # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
     
