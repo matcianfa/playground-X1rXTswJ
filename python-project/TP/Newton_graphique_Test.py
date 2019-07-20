95b557566969fadda1c7c3812b3432ab4d11d457
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-20 10:17:27 +0200

    Update Newton_graphique_Test.py

--- python-project/TP/Newton_graphique_Test.py
+++ python-project/TP/Newton_graphique_Test.py
@@ -2,6 +2,20 @@
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+# Les imports
+import sys
+# On rajoute le chemin de ma_bao.py dans le sys.path
+sys.path.append("/project/target")
+# Ma boite à outils
+from ma_bao import * 
+# Donne les noms du dossier et du module (automatiquement avec __file__
+chemin,module=donner_chemin_nom(__file__)
+# On teste s'il n'y a pas d'erreurs de synthaxe etc. et on les montre si besoin
+tester("from {} import *".format(module),globals()) 
+# Si le mot de passe est bon on affiche la correction
+try :  
+    cheat(chemin+module,mdp) 
+except: pass
 
 fig=plt.figure()
 import Newton_graphique
