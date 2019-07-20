2c6a24a80595527bb970e16dc1fe783eccf0a0d0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-20 10:15:33 +0200

    Update Newton_graphique.py

--- python-project/TP/Newton_graphique.py
+++ python-project/TP/Newton_graphique.py
@@ -3,9 +3,29 @@ import numpy as np
 
 # La fonction f à tracer
 def f(x) :
-    return x**2 - 2
+    return x**4 - 2
     
 # La valeur initiale
 x0 = 3
 
+# Intervalle sur lequel on observe la fonction
+x_min = 1
+x_max = 3.1
+
+# On trace l'axe des abscisses
+plt.plot([x_min,x_max],[0,0],"k",linewidth=1)
+
 # Ecrire ci dessous votre script pour qu'il affiche la fonction et les premières tangentes de la méthode de Newton
+
+
+
+
+
+
+
+
+
+
+
+# Ne rien écrire après
+plt.show()
