75f38b91ab25be5307d1cad18c94e1a3357cf2fa
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-21 11:42:15 +0200

    Update approx_pi_2_Correction.py

--- python-project/TP/approx_pi_2_Correction.py
+++ python-project/TP/approx_pi_2_Correction.py
@@ -1,5 +1,5 @@
 def ma_fonction(n):
     c = 2**0.5
     for i in range(n-2): # On part de c_2 donc pour arrive à c_n il faut n-2 étapes
-        c = (c*c/4+(1-(1-c*c/4)**2)**0.5)**0.5
+        c = (c*c/4+(1-(1-c*c/4)**0.5)**2)**0.5
     return n*c/2
