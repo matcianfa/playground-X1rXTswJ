d092a3c883d1e53cbc76b06e088cf8ba4e88b8e1
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-21 12:52:51 +0200

    Create mesure_principale3_Correction.py

---
+++ python-project/Premiere/Derivation/mesure_principale3_Correction.py
@@ -0,0 +1,10 @@
+from math import pi
+from fractions import Fraction
+
+def ma_fonction(angle):
+    angle/=pi
+    while angle > 1 :
+        angle -= 2
+    while angle <= -1:
+        angle += 2
+    return "("+str(Fraction(angle).limit_denominator())+")pi"
