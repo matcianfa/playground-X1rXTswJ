906bcb99870cdae8dd28b7deefaebe597f879dff
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-21 12:36:01 +0200

    Create mesure_principale2.py

---
+++ python-project/Premiere/Derivation/mesure_principale2.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def ma_fonction(angle):
+    # Ne pas toucher ce qui précède
+    # Les valeurs pour les variables en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+
+
+
