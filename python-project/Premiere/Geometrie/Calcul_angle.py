3bf2439612e7e04c6f50482e0b3d19fb98b57974
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-22 17:14:26 +0200

    Update Calcul_angle.py

--- python-project/Premiere/Geometrie/Calcul_angle.py
+++ python-project/Premiere/Geometrie/Calcul_angle.py
@@ -1,6 +1,5 @@
-def ma_fonction(BC,AC,AB):
-    # Ne pas toucher ce qui précède
-    # Les valeurs pour les variables en entrée seront automatiquement données
-    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
-    
+from math import *
 
+def ma_fonction(BC,AC,AB):
+    c = (...**2 + ...**2 - ...**2)/(2 * ... * ...)
+    return acos(...)
