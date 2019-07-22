0f16f2b1fbd45d340b0f1e81db66de4b3aaedc57
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-22 16:42:20 +0200

    Create Calcul_du_produit_scalaire2_Correction.py

---
+++ python-project/Premiere/Geometrie/Calcul_du_produit_scalaire2_Correction.py
@@ -0,0 +1,11 @@
+def ma_fonction(u1,u2):
+    return u1[0]*u2[0] + u1[1]*u2[1]
+    
+"""
+Autre possibilité :
+
+def ma_fonction(u1,u2):
+    x1,y1 = u1
+    x2,y2 = u2
+    return  x1*x2 + y1*y2
+"""
