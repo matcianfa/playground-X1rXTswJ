2422572534e1771d9a78e34e11b4d89f53251da7
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-23 16:36:06 +0200

    Create occurrence.py

---
+++ python-project/TP/occurrence.py
@@ -0,0 +1,8 @@
+def compter(lettre,texte):
+    # Ne pas toucher ce qui précède
+    # Les valeurs pour les variables en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+    
+    
+   
