55c3f6ff29cebcdd3f1e8b53433d71deaef98598
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-23 20:28:39 +0200

    Update simulations_Test.py

--- python-project/TP/simulations_Test.py
+++ python-project/TP/simulations_Test.py
@@ -1,7 +1,7 @@
 from Euromillion import *
 
 if len(mes_numeros)!=5 or len(mes_etoiles)!=2 or len(set(mes_numeros))!=5 or len(set(mes_etoiles))!=2 or not all([0<n<51 for n in mes_numeros]) or not all([0<e<12 for e in mes_etoiles]) : 
-    send_msg("Oops! ", "La variable mes_numeros ou mes_etoiles n'est pas valide)
+    send_msg("Oops! ", "La variable mes_numeros ou mes_etoiles n'est pas valide")
     print("TECHIO> success false")
 else : 
     simulation()
