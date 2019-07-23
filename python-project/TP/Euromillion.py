c9d2b51889384fbf9ab3254c45f35f48909e12c3
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-23 19:17:45 +0200

    Create Euromillion.py

---
+++ python-project/TP/Euromillion.py
@@ -0,0 +1,33 @@
+from random import randint
+
+# Vous pouvez modifier ces listes pour choisir vos propres numéros. 
+mes_numeros = [1,2,3,4,5]
+mes_etoiles = [1,2]
+
+prix_ticket = 2.5
+
+# Le nombre de simulations faites (utile uniquement à la fin). Vous pouvez le modifier.
+nb_simulations = 1000000
+
+
+def tirages():
+    tirage=[]
+    for i in range(...):
+        boule = randint(1,50)
+        while boule in tirage :
+            boule = ...
+        tirage.append(boule)
+        
+    etoiles = []
+    for i in range(...):
+        etoile = ...
+        while etoile in ... :
+            etoile = ...
+        etoiles.append(...)
+        
+    return tirage,etoiles
+    
+    
+    
+    
+    
