49eabed411cae7f7ffcaf0a9a18ed51d6483a5e0
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-26 17:49:26 +0200

    Create Hofstadter_graphique.py

---
+++ python-project/TP/Hofstadter_graphique.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# ---------- Les paramètres :
+# On calcule les termes de Q de 1 jusqu'à N
+N = 10000
+# nb_termes correspond au nombre de terme qu'on veut afficher : de N-nb_termes+1 à N
+nb_termes = 50
+
+# Copier coller la fonction Q ci dessous
+
+
+
+
+# Compléter la valeur de Y pour n'afficher que les termes de la suite de N-nb_termes+1 à N
+X = list(range(50))
+Y = ...
+
+plt.plot(X,Y,"-",linewidth=1)
+plt.show()
