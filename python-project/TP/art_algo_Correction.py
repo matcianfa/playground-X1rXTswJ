4cd8200a66f6ec8a636bf3b87ee8ead12f3141c6
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-26 19:43:33 +0200

    Create art_algo_Correction.py

---
+++ python-project/TP/art_algo_Correction.py
@@ -0,0 +1,19 @@
+from math import *
+
+def u(n) :
+    if n== 0 : return 0
+    return (n+0.15)*n**0.5
+    
+    
+def x(n):
+    liste = [0]
+    for i in range(1,n+1):
+        liste.append(liste[i-1]+ cos(2*pi*u(i)))
+    return liste
+    
+
+def y(n):
+    liste = [0]
+    for i in range(1,n+1):
+        liste.append(liste[i-1]+ sin(2*pi*u(i)))
+    return liste
