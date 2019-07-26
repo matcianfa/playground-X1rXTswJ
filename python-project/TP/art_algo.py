aab3ad76a2ed8244180a4fddd80338ac161f9417
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-26 19:32:25 +0200

    Create art_algo.py

---
+++ python-project/TP/art_algo.py
@@ -0,0 +1,17 @@
+def u(n):
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+    
+
+def x(n):
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+
+
+
+
+def y(n):
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+
+  
+  
+  
