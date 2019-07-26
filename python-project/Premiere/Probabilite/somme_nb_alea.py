2fa952e291f94df51a3b3e07351233a228274b7f
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-26 18:41:01 +0200

    Create somme_nb_alea.py

---
+++ python-project/Premiere/Probabilite/somme_nb_alea.py
@@ -0,0 +1,15 @@
+from random import random
+
+# Nombres d'essais à faire :
+N = 10000000
+
+def ma_fonction() :
+    # Ne pas toucher ce qui précède
+    # Les valeurs pour les variables en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+    
+    
+
+    
+   
