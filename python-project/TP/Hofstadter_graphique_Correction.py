f19458293a1a2373f07b872313e7f21c0ee87cc9
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-26 17:51:47 +0200

    Create Hofstadter_graphique_Correction.py

---
+++ python-project/TP/Hofstadter_graphique_Correction.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# ---------- Les paramètres :
+# On calcule les termes de Q de 1 jusqu'à N
+N = 10000
+# nb_termes correspond au nombre de terme qu'on veut afficher : de N-nb_termes+1 à N
+nb_termes = 50
+
+# Copier coller la fonction Q ci dessous
+def Q(n):
+    liste = [0,1,1] # On met 0 en indice 0 juste pour décaler pour que les indices soient corrects ( pour que Q(1)=liste[1] ...)
+    for i in range(3,n+1):
+        liste.append(liste[i-liste[i-1]]+liste[i-liste[i-2]])
+    return liste[1:]
+
+# Compléter la valeur de Y pour n'afficher que les termes de la suite de N-nb_termes+1 à N
+X = list(range(50))
+Y = Q(n)[-50:]
+
+plt.plot(X,Y,"-",linewidth=1)
+plt.show()
