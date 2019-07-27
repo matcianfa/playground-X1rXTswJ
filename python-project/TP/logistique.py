01f3721f87c5fc510d590a5c9b655a8f038f03e3
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-27 08:15:35 +0200

    Update logistique.py

--- python-project/TP/logistique.py
+++ python-project/TP/logistique.py
@@ -1,6 +1,8 @@
-def u(lambda,n):
+def u(mu,u0,n):
     # Ne pas toucher ce qui précède
     # Les valeurs pour les variables en entrée seront automatiquement données
     # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
     
 
+    
+    
