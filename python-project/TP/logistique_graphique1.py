257ec9766400fca0a8f1a0010b98222c5cd51823
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-27 09:27:49 +0200

    Create logistique_graphique1.py

---
+++ python-project/TP/logistique_graphique1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Copier coller ci dessous votre fonction u(mu,u0,n)
+
+
+
+
+
+def dessiner(mu,u0,n):
+    # Les valeurs pour les variables en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter
+    
+    
+    
+
+  
