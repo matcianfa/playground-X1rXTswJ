ec5aa51c279fe07e1597f12ae55b26eb4739dde9
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-28 09:16:19 +0200

    Create logistique_graphique3.py

---
+++ python-project/TP/logistique_graphique3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Copier coller votre fonction u(mu,u0,n)
+
+
+
+
+# Copier coller votre fonction dessiner(mu,u0,n) puis la modifier pour afficher les deux cas demandés
+
+
+
+
