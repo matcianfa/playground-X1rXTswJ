cba55f60802e53b1a90b5a3133a06cde38aba5cb
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-28 12:31:15 +0200

    Create logistique_complexe_graphique.py

---
+++ python-project/TP/logistique_complexe_graphique.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def u(mu,n) :
+    # Créer votre fonction u(mu,n) ici. Ne pas oublier d'indenter
+    
+    
+    
+
+
+def dessiner(mu,n) :
+    # Compléter la fonction dessiner(mu,n). 
+    # Ce qui suit est une proposition de début de code, vous pouvez rajouter des lignes si necessaire.
+    
+    X = ...
+    Y = ...
+    plt.plot(X,Y,".-",linewidth=1)
+    plt.show()
+    
+    
