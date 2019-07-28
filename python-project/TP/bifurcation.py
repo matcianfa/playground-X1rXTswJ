e5177c0699527399e81ae1bbac266e166ac13782
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-07-28 10:04:38 +0200

    Create bifurcation.py

---
+++ python-project/TP/bifurcation.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+# Copier Coller votre fonction u(mu,u0,n) ci dessous
+
+
+
+
+
+
+mu=2
+while mu < ... :
+    X = ...
+    Y = ...
+    plt.plot(X,Y,"r,")
+    mu+=0.001
+    
+plt.show()
