246bf1e0ba61ba0cdda17cd3a1dda215ad8d0fef
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-08-01 17:04:47 +0200

    Create Julia.py

---
+++ python-project/TP/Julia.py
@@ -0,0 +1,9 @@
+MU = complex(-1, 0.05)
+
+def Julia(mu,u0):
+    # Ne pas toucher ce qui précède
+    # Les valeurs pour les variables en entrée seront automatiquement données
+    # Ecrire ci-dessous en n'oubliant pas d'indenter et d'utiliser return pour renvoyer un résultat
+    
+    
+    
