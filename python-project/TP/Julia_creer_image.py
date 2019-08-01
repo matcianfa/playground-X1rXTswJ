42dbf55f2e05f3d9dca67ba6ce0ab7d5ca1c5a7a
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-08-01 17:07:20 +0200

    Create Julia_creer_image.py

---
+++ python-project/TP/Julia_creer_image.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw  # Module de gestion des images
+import numpy as np
+from Julia import Julia, MU
+
+# --------- Les constantes, modifiables
+WIDTH,HEIGHT = 900,600
+X_MIN,Y_MIN,X_MAX = -1,-1,4 # Valeurs min et max pour les parties reelles et imaginaires
+Y_MAX=Y_MIN+HEIGHT*(X_MAX-X_MIN)/WIDTH #Pour avoir un repère normé
+
+
+def creer_image(mu=MU):
+    im = Image.new('HSV', (WIDTH, HEIGHT), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    X = np.linspace(X_MIN, X_MAX, WIDTH)
+    Y = np.linspace(Y_MIN, Y_MAX, HEIGHT)
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            n=Julia(mu,complex(X[x],Y[y]))
+            draw.point((x, y), (n%255, 255, 255 if n < MAX_ITER else 0))
+    im.convert('RGB').save('output.png', 'PNG')
