d2b926e226455b34d39b14231f0963788e920ce5
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-08-14 15:48:32 +0200

    Update mandelbrot_numpy.py

--- python-project/Optimisation/mandelbrot_numpy.py
+++ python-project/Optimisation/mandelbrot_numpy.py
@@ -19,13 +19,10 @@ def mandelbrot(c):
     """
     resultats = np.zeros(c.shape) # On initialise à 0.
     z = np.zeros(c.shape, dtype = np.complex64) # On initialise les z
-    p = np.abs(c-0.25)
-    cardioide = (c.real < p - 2*p*p + 0.25) | ((c.real+1)**2 + c.imag**2 < 0.0625) # Pour garder en mémoire les éléments dans la cardioide
-    resultats[cardioide]=1 # Les points de la cardioides et du cercles sont sur l'ensemble de Mandelbrot
-    pas_fini =  ~(cardioide) # Un tableau pour savoir pour quels indices il y a encore des calculs à faire. Initialisation à True sauf pour les c qui sont dans la cardioide ou le cercle.
+    pas_fini =  resultats == 0 # Un tableau pour savoir pour quels indices il y a encore des calculs à faire. Initialisation à True sauf pour les c qui sont dans la cardioide ou le cercle.
     for n in range(MAX_ITER):
         # Pour éviter de calculer pour tous les z, on cherche ceux qui ont encore un module inférieur à 2
-        pas_fini = pas_fini & (np.abs(z)< 2)
+        pas_fini = np.abs(z)< 2
         z[pas_fini] = z[pas_fini]**2 + c[pas_fini] # On ne calcule que pour les indices où il y a encore des calculs à faire (indiqué par la tableau pas_fini)
     resultats[pas_fini] = 1 # Si pas fini c'est qu'on est dans l'ensemble de Mandelbrot
     return resultats
