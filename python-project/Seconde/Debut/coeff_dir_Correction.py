88a1659d826f8bd78c3850af4a759a46825874d1
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-10-20 18:03:06 +0200

    Update coeff_dir_Correction.py

--- python-project/Seconde/Debut/coeff_dir_Correction.py
+++ python-project/Seconde/Debut/coeff_dir_Correction.py
@@ -1,7 +1,7 @@
 def ma_fonction(xA,yA,xB,yB):
     return (yB-yA)/(xB-xA)
     
-# S'il on avait voulu considérer la possibilité d'une division par 0,
+# Si on avait voulu considérer la possibilité d'une division par 0,
 # On aurait pu faire : 
 '''
 def ma_fonction(xA,yA,xB,yB):
