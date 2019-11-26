1aa4a2568c9e3cbb84d65f85844d90a6fd41972d
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-11-26 18:49:46 +0100

    Update Manip_image_base.py

--- python-project/Info/Manip_image_base.py
+++ python-project/Info/Manip_image_base.py
@@ -5,10 +5,10 @@ import numpy as np
 image_entrée = Image.open("Info/Lenna.png")
 image = np.asarray(image_entrée)
 
-# On crée notre image de sortie (ici on fait juste une copie de l'image originale)
-image_sortie =Image.fromarray(image)
+# On crée notre image de sortie sous forme de tableau numpy (ici on fait juste une copie de l'image originale)
+image_sortie =image
 
 # On sauvegarde les images pour pouvoir les afficher
 Image.fromarray(image).save("image_entree.png")
-image_sortie.save("image_sortie.png")
+Image.fromarray(image_sortie).save("image_sortie.png")
 
