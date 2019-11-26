ee85beee29852b88ad967bd3e41490dcf26ebe70
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-11-26 18:48:57 +0100

    Create Manip_image_rogner.py

---
+++ python-project/Info/Manip_image_rogner.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw
+import numpy as np
+
+# On charge l'image et on la transforme en tableau contenant les couleurs
+image_entrée = Image.open("Info/Lenna.png")
+image = np.asarray(image_entrée)
+
+# On crée notre image de sortie (ici on fait juste une copie de l'image originale)
+image_sortie = ...
+
+# On sauvegarde les images pour pouvoir les afficher
+Image.fromarray(image).save("image_entree.png")
+Image.fromarray(image_sortie).save("image_sortie.png")
