5124e8e6229ff70711e44d81a9b36c5739ce7963
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-11-28 19:27:33 +0100

    Create Manip_image_mono_couleur.py

---
+++ python-project/Info/Manip_image_mono_couleur.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageDraw
+import numpy as np
+
+# On charge l'image et on la transforme en tableau contenant les couleurs
+image_entrée = Image.open("Info/Lenna.png")
+image = np.asarray(image_entrée)
+nb_lignes,nb_colonnes,_ = image.shape
+
+# Partie à compléter
+image_sortie = np.copy(image)
+for ligne in range(nb_lignes):
+    for col in range(nb_colonnes):
+        image_sortie[ligne,col] = ...
+
+# On sauvegarde les images pour pouvoir les afficher
+Image.fromarray(image).save("image_entree.png")
+Image.fromarray(image_sortie).save("image_sortie.png")
