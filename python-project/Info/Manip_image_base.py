95557c9c2d55a318c5bb94d25a9fd964c84c6e3e
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-11-28 18:42:54 +0100

    Update Manip_image_base.py

--- python-project/Info/Manip_image_base.py
+++ python-project/Info/Manip_image_base.py
@@ -4,6 +4,7 @@ import numpy as np
 # On charge l'image et on la transforme en tableau contenant les couleurs
 image_entrée = Image.open("Info/Lenna.png")
 image = np.asarray(image_entrée)
+nb_lignes,nb_colonnes,_ = image.shape
 
 # On crée notre image de sortie sous forme de tableau numpy (ici on fait juste une copie de l'image originale)
 image_sortie =np.copy(image)
@@ -11,4 +12,3 @@ image_sortie =np.copy(image)
 # On sauvegarde les images pour pouvoir les afficher
 Image.fromarray(image).save("image_entree.png")
 Image.fromarray(image_sortie).save("image_sortie.png")
-
