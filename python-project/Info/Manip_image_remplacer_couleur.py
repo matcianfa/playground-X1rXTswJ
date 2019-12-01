219f8aa0ccdadac6b662a91e5fbb965bc921c498
matcianfa <36672244+matcianfa@users.noreply.github.com>
2019-12-01 19:48:01 +0100

    Create Manip_image_remplacer_couleur.py

---
+++ python-project/Info/Manip_image_remplacer_couleur.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw
+import numpy as np
+
+# On charge l'image et on la transforme en tableau contenant les couleurs
+image_entrée = Image.open("Info/Lenna.png")
+image = np.asarray(image_entrée)
+
+# Partie à compléter
+image_sortie = np.copy(image)
+for ligne in range(nb_lignes):
+    for col in range(nb_colonnes):
+        image_sortie[ligne,col] = ...
+
+# On sauvegarde les images pour pouvoir les afficher
+Image.fromarray(image).save("image_entree.png")
+Image.fromarray(image_sortie).save("image_sortie.png")
