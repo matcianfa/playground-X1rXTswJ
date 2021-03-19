8af01fc359870fffa9938c7ee811f7054fabef23
matcianfa <36672244+matcianfa@users.noreply.github.com>
2021-03-19 12:40:17 +0100

    Create fonction_afficher_jouer_son.py

---
+++ python-project/Physique/fonction_afficher_jouer_son.py
@@ -0,0 +1,18 @@
+import soundfile as sf
+from matplotlib import pyplot as plt
+import numpy as np
+
+def afficher_jouer_son(son,frequence,fichier = "Physique/son.wav"):
+  # On l'affiche
+  taille = son.shape[0]
+  temps = np.arange(0,taille)/frequence
+  plt.plot(temps,son)
+  plt.show()
+  # On l'enregistre pour qu'il se joue automatiquement dans la page html 
+  # (pirouette due au fait de passer par ce site)
+  # Sur son pc il suffirait de faire 
+
+  #import sounddevice as sd
+  #sd.play(son,frequence)
+  
+  sf.write(fichier,son,frequence)
