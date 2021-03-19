daf0d9f2f2ed976f49a6b5457b77cd0d8c52fbbc
matcianfa <36672244+matcianfa@users.noreply.github.com>
2021-03-19 15:21:35 +0100

    Create creer_un_son_Test.py

---
+++ python-project/Physique/creer_un_son_Test.py
@@ -0,0 +1,45 @@
+#message d'aide si besoin
+help=""
+
+# Les imports
+import matplotlib
+matplotlib.use('agg')
+import sys
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+# On rajoute le chemin de ma_bao.py dans le sys.path
+sys.path.append("/project/target")
+# Ma boite à outils
+from ma_bao import * 
+# Donne les noms du dossier et du module (automatiquement avec __file__
+chemin,module=donner_chemin_nom(__file__)
+
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+
+
+def success():
+    print("TECHIO> open -s /project/target/Physique afficher_jouer_son.html")
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+    
+
+def test():
+    try:
+      fig=plt.figure()
+      exec("import {}".format(module)) 
+      fig.savefig('Physique/output.png', dpi=fig.dpi)
+      success()
+    except AssertionError as e:
+      fail()
+      send_msg("Oops! ", e)
+      if help:
+        send_msg("Aide 💡", help)
+
+
+if __name__ == "__main__": test()
