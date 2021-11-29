9b02b818c9eee70ec38c7a6e6fc222c1d3b47491
matcianfa <36672244+matcianfa@users.noreply.github.com>
2021-11-29 17:13:42 +0100

    Update Calcul_somme_geometrique_Test.py

--- python-project/Premiere/Suites/Calcul_somme_geometrique_Test.py
+++ python-project/Premiere/Suites/Calcul_somme_geometrique_Test.py
@@ -33,8 +33,8 @@ exec("from {}_Correction import {} as f_sol".format(module,nom_fonction))
 def test():
     try:
         for valeur in valeurs_a_tester:
-            rep=f(*valeur)
-            sol=int(f_sol(*valeur))
+            rep=float(f(*valeur))
+            sol=float(f_sol(*valeur))
             assert str(rep) == str(sol), "En testant les valeurs {} le résultat obtenu est {} au lieu de {}".format(",".join([str(val) for val in valeur]),str(rep),str(sol))
             send_msg("Tests validés","En testant les valeurs {} le résultat obtenu est bien {}".format(",".join([str(val) for val in valeur]),str(rep)))
         success(chemin+module)
