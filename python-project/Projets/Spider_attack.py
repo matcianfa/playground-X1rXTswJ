66e4c76c8005b10d501a5dcf67e7a77349402791
matcianfa <36672244+matcianfa@users.noreply.github.com>
2023-02-23 18:40:22 +0100

    Create Spider_attack.py

---
+++ python-project/Projets/Spider_attack.py
@@ -0,0 +1,81 @@
+import sys
+from collections import namedtuple
+
+# ----------- Fonctions Auxiliaires
+
+def log(*x):
+    """
+    Permet d'afficher des données utiles comme un message d'erreur (et donc qui ne sera pas considéré comme une réponse)
+    Ex : log(a,b,c) affichera les valeurs de a, b et c
+    """
+    print(*x, file=sys.stderr, flush = True)
+
+
+# ----------- Constantes et variables
+
+TYPE_MONSTRE = 0
+TYPE_MON_HEROS = 1
+TYPE_ADV_HEROS = 2
+
+
+Agent = namedtuple('Agent', [
+    'id', 'type', 'x', 'y', 'duree_bouclier', 'sous_controle', 'vie', 'vx', 'vy', 'proche_base', 'menace_pour'
+])
+
+
+
+# Les coordonnées du coin représentant notre base
+ma_base_x, ma_base_y = [int(i) for i in input().split()]
+heros_par_joueur = int(input())
+
+# ------------ Boucle du jeu
+while True:
+
+    # -------- Récupération des données pour ce tour de jeu
+
+    ma_vie, mon_mana = [int(j) for j in input().split()]
+    vie_adv, mana_adv = [int(j) for j in input().split()]
+    nombre_agents_visibles = int(input())  # Nombres de heros et monstres visibles
+
+    monstres = []
+    mes_heros = []
+    adv_heros = []
+
+    for i in range(nombre_agents_visibles):
+        _id, _type, x, y, duree_bouclier, sous_controle, vie, vx, vy, proche_base, menace_pour = [int(j) for j in input().split()]
+        entity = Agent(
+            _id,            # _id: Identifiant unique
+            _type,          # _type: 0 = monstre, 1 = mon heros, 2 = heros adverse
+            x, y,           # x,y: Position de l'agent
+            duree_bouclier, # duree_bouclier: Inutile pour la première ligue. Nombre de tours avant que le bouclier disparaisse
+            sous_controle,  # sous_controle: Inutile pour la première ligue. Vaut 1 quand cet agent est sous controle d'un sort
+            vie,            # vie: Vie restante de l'agent
+            vx, vy,         # vx,vy: Coordonnées du vecteur vitesse de l'agent
+            proche_base,    # proche_base: 0 = Le monstre n'a pas de cible, 1 = Le monstre cible une base
+            menace_pour     # menace_pour : Etant donné la trajectoire du monstre, le monstre est une menace pour 1 = votre base, 2 = la base adverse, 0 = personne
+        )
+        
+        if _type == TYPE_MONSTRE:
+            monstres.append(entity)
+        elif _type == TYPE_MON_HEROS:
+            mes_heros.append(entity)
+        elif _type == TYPE_ADV_HEROS:
+            adv_heros.append(entity)
+
+
+    # ------------------------------------------
+    # ------------- Logique du jeu -------------
+    # ------------------------------------------
+
+    for numero_heros in range(heros_par_joueur):
+
+        cible = None
+        if monstres:
+            cible = monstres[0]
+        
+        
+        if cible:
+            print(f'MOVE {cible.x} {cible.y}')
+        else:
+            print('WAIT')
+            
